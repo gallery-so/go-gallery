3c495ecf3082a0eec0cdc9579aa81601446a4f0f
bennycio <benny@bennyc.io>
2021-07-11 20:03:20 -0700

    update gallery funcs to reflect new mongo wrappers

--- glry_db/glry_gallery.go
+++ glry_db/glry_gallery.go
@@ -2,15 +2,14 @@ package glry_db
 
 import (
 	"context"
-	"crypto/md5"
-	"encoding/hex"
-	"fmt"
+	"time"
 
-	"github.com/gloflow/gloflow/go/gf_core"
 	"github.com/mikeydub/go-gallery/glry_core"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+const galleryColName = "glry_galleries"
 
 //-------------------------------------------------------------
 type GLRYgalleryID string
@@ -27,95 +26,52 @@ type GLRYgallery struct {
 //-------------------------------------------------------------
 func GalleryCreate(pGallery *GLRYgallery,
 	pCtx context.Context,
-	pRuntime *glry_core.Runtime) *gf_core.Gf_error {
+	pRuntime *glry_core.Runtime) error {
 
-	collNameStr := "glry_galleries"
-	gErr := gf_core.Mongo__insert(pGallery,
-		collNameStr,
-		map[string]interface{}{
-			"gallery_owner":  pGallery.OwnerUserIDstr,
-			"caller_err_msg": "failed to insert a new gallery into the DB",
-		},
-		pCtx,
-		pRuntime.RuntimeSys)
-	if gErr != nil {
-		return gErr
-	}
+	mp := glry_core.NewMongoPersister(0, collectionColName, pRuntime)
 
-	return nil
+	return mp.Insert(pCtx, pGallery)
 }
 
 //-------------------------------------------------------------
 func GalleryGetByUserID(pUserIDstr GLRYuserID,
 	pCtx context.Context,
-	pRuntime *glry_core.Runtime) ([]*GLRYcollection, *gf_core.Gf_error) {
+	pRuntime *glry_core.Runtime) ([]*GLRYgallery, error) {
 
-	find_opts := options.Find()
-	c, gErr := gf_core.MongoFind(bson.M{
-		"owner_user_id": pUserIDstr,
-		"deleted":       false,
-	},
-		find_opts,
-		map[string]interface{}{
-			"owner_user_id":      pUserIDstr,
-			"caller_err_msg_str": "failed to get galleries from DB by user_id",
-		},
-		pRuntime.RuntimeSys.Mongo_db.Collection("glry_galleries"),
-		pCtx,
-		pRuntime.RuntimeSys)
-
-	if gErr != nil {
-		return nil, gErr
+	opts := &options.FindOptions{}
+	if deadline, ok := pCtx.Deadline(); ok {
+		dur := time.Until(deadline)
+		opts.MaxTime = &dur
 	}
 
-	var collsLst []*GLRYcollection
-	err := c.All(pCtx, collsLst)
-	if err != nil {
-		gf_err := gf_core.Mongo__handle_error("failed to decode mongodb result of query to get galleries",
-			"mongodb_cursor_decode",
-			map[string]interface{}{},
-			err, "gf_eth_monitor_core", pRuntime.RuntimeSys)
+	mp := glry_core.NewMongoPersister(0, collectionColName, pRuntime)
 
-		return nil, gf_err
+	result := []*GLRYgallery{}
+
+	if err := mp.Find(pCtx, bson.M{"user_id": pUserIDstr}, result, opts); err != nil {
+		return nil, err
 	}
 
-	return collsLst, nil
+	return result, nil
 }
 
 //-------------------------------------------------------------
 func GalleryGetByID(pIDstr string,
 	pCtx context.Context,
-	pRuntime *glry_core.Runtime) (*GLRYcollection, *gf_core.Gf_error) {
-
-	var coll *GLRYcollection
-	err := pRuntime.RuntimeSys.Mongo_db.Collection("glry_galleries").FindOne(pCtx, bson.M{
-		"_id":     pIDstr,
-		"deleted": false,
-	}).Decode(&coll)
-
-	if err != nil {
-		gf_err := gf_core.Mongo__handle_error("failed to query gallery by ID",
-			"mongodb_find_error",
-			map[string]interface{}{"id": pIDstr},
-			err, "glry_db", pRuntime.RuntimeSys)
-		return nil, gf_err
+	pRuntime *glry_core.Runtime) ([]*GLRYgallery, error) {
+	opts := &options.FindOptions{}
+	if deadline, ok := pCtx.Deadline(); ok {
+		dur := time.Until(deadline)
+		opts.MaxTime = &dur
 	}
 
-	return coll, nil
-}
+	mp := glry_core.NewMongoPersister(0, collectionColName, pRuntime)
 
-//-------------------------------------------------------------
-// CREATE_ID
-func GalleryCreateId(pNameStr string,
-	pOwnerUserIDstr string,
-	pCreationTimeUNIXf float64) GLRYcollID {
+	result := []*GLRYgallery{}
 
-	h := md5.New()
-	h.Write([]byte(fmt.Sprint(pCreationTimeUNIXf)))
-	h.Write([]byte(pNameStr))
-	h.Write([]byte(pOwnerUserIDstr))
-	sum := h.Sum(nil)
-	hexStr := hex.EncodeToString(sum)
-	ID := GLRYcollID(hexStr)
-	return ID
+	if err := mp.Find(pCtx, bson.M{"_id": pIDstr}, result, opts); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
