d6d58e3e3cb9b0626c814f19167d5ec0179f506b
Fabric Softener <fabricsoftener3@gmail.com>
2021-07-18 23:29:05 -0400

    integration tests for getNftById

--- server/t__routes_test.go
+++ server/t__routes_test.go
@@ -1,12 +1,62 @@
 package server
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"testing"
 
+	"github.com/mikeydub/go-gallery/persist"
 	"github.com/mikeydub/go-gallery/runtime"
 )
+
+func TestGetNftById_Success(t *testing.T) {
+	assert, testServer, serverUrl, r := setup(t)
+	defer teardown(testServer)
+
+	// seed DB with nft
+	name := "very cool nft"
+	nftId, err := persist.NftCreate(&persist.Nft{
+		NameStr: name,
+	}, context.Background(), r)
+	assert.Nil(err)
+
+	resp, err := http.Get(fmt.Sprintf("%s/nfts/get?id=%s", serverUrl, nftId))
+	assert.Nil(err)
+	assertValidJSONResponse(assert, resp)
+
+	body := persist.Nft{}
+	runtime.UnmarshalBody(&body, resp.Body, r)
+	assert.Equal(name, body.NameStr)
+}
+
+func TestGetNftById_NoParamError(t *testing.T) {
+	assert, testServer, serverUrl, r := setup(t)
+	defer teardown(testServer)
+
+	resp, err := http.Get(fmt.Sprintf("%s/nfts/get", serverUrl))
+	assert.Nil(err)
+	assertGalleryErrorResponse(assert, resp)
+
+	body := ErrorResponse{}
+	runtime.UnmarshalBody(&body, resp.Body, r)
+	assert.Equal(nftIdQueryNotProvided, body.Error)
+}
+
+func TestGetNftById_NotFoundError(t *testing.T) {
+	assert, testServer, serverUrl, r := setup(t)
+	defer teardown(testServer)
+
+	nonexistentNftId := "12345"
+
+	resp, err := http.Get(fmt.Sprintf("%s/nfts/get?id=%s", serverUrl, nonexistentNftId))
+	assert.Nil(err)
+	assertGalleryErrorResponse(assert, resp)
+
+	body := ErrorResponse{}
+	runtime.UnmarshalBody(&body, resp.Body, r)
+	assert.Equal(fmt.Sprintf("no nfts found with id: %s", nonexistentNftId), body.Error)
+}
 
 func TestHealthcheck(t *testing.T) {
 	assert, testServer, serverUrl, r := setup(t)
@@ -18,7 +68,6 @@ func TestHealthcheck(t *testing.T) {
 
 	body := HealthcheckResponse{}
 	runtime.UnmarshalBody(&body, resp.Body, r)
-
-	assert.Equal(body.Message, "gallery operational")
-	assert.Equal(body.Env, "local")
+	assert.Equal("gallery operational", body.Message)
+	assert.Equal("local", body.Env)
 }
