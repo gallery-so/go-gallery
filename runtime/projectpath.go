634be82ab888ec33029ea3256aa364ac25c97de5
kaito <80802871+kaitoo1@users.noreply.github.com>
2021-07-21 14:36:10 +0900

    Move .env to bin and flexibily point to .env on startup

--- runtime/projectpath.go
+++ runtime/projectpath.go
@@ -3,6 +3,8 @@ package runtime
 import (
 	"path/filepath"
 	"runtime"
+	"os"
+	"fmt"
 )
 
 var (
@@ -11,3 +13,17 @@ var (
 	// Root folder of this project
 	ProjectRootPath = filepath.Join(filepath.Dir(b), "..")
 )
+
+func fileExists(path string) bool {
+	_, err := os.Stat(path)
+	return !os.IsNotExist(err)
+}
+
+func getEnvPath() string {
+	envPath := ".env"
+	envIsLocal := fileExists(envPath)
+	if !envIsLocal {
+		envPath = fmt.Sprintf("%s/bin/.env", ProjectRootPath)
+	}
+	return envPath
+}
