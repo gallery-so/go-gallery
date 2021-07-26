4ca18ccc975b0980bc23d27a03556b9fc90bb563
bennycio <benny@bennyc.io>
2021-07-25 22:52:15 -0700

    validators don't check empty fields, thats for required validator to do

--- server/validate.go
+++ server/validate.go
@@ -8,17 +8,26 @@ import (
 
 var ethValidator validator.Func = func(fl validator.FieldLevel) bool {
 	addr := fl.Field().String()
+	if addr == "" {
+		return true
+	}
 	return len(addr) == 42 && strings.HasPrefix(addr, "0x")
 }
 
 var signatureValidator validator.Func = func(fl validator.FieldLevel) bool {
 	sig := fl.Field().String()
+	if sig == "" {
+		return true
+	}
 	return len(sig) >= 80 && len(sig) <= 200
 }
 
 var nonceValidator validator.Func = func(fl validator.FieldLevel) bool {
-	sig := fl.Field().String()
-	return len(sig) >= 10 && len(sig) <= 150
+	nonce := fl.Field().String()
+	if nonce == "" {
+		return true
+	}
+	return len(nonce) >= 10 && len(nonce) <= 150
 }
 
 var shortStringValidator validator.Func = func(fl validator.FieldLevel) bool {
