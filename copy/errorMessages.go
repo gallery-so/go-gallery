0d5e88efd41c6763526ae5c23013eaf97ab24f7f
Fabric Softener <fabricsoftener3@gmail.com>
2021-07-28 02:40:01 -0400

    copy package

---
+++ copy/errorMessages.go
@@ -0,0 +1,9 @@
+package copy
+
+const (
+	// request
+	InvalidAuthHeader = "invalid authorization header format"
+	NftIdQueryNotProvided = "nft id not provided in query values"
+	// db
+	CouldNotFindDocument = "could not find document"
+)
