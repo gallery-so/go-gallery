cd316ca5c9be265b47cb61781817c019f4cdf634
bennycio <benny@bennyc.io>
2021-08-17 22:43:55 -0700

    no colls, return all nfts

--- persist/collection.go
+++ persist/collection.go
@@ -184,8 +184,21 @@ func CollGetUnassigned(pCtx context.Context, pUserID DBID, skipCache bool, pRunt
 		}
 	}
 
-	if err := mp.aggregate(pCtx, newUnassignedCollectionPipeline(pUserID), &result, opts); err != nil {
+	countColls, err := mp.count(pCtx, bson.M{"owner_user_id": pUserID})
+	if err != nil {
 		return nil, err
+	}
+
+	if countColls == 0 {
+		nfts, err := NftGetByUserID(pCtx, pUserID, pRuntime)
+		if err != nil {
+			return nil, err
+		}
+		result = []*Collection{{Nfts: nfts}}
+	} else {
+		if err := mp.aggregate(pCtx, newUnassignedCollectionPipeline(pUserID), &result, opts); err != nil {
+			return nil, err
+		}
 	}
 	if len(result) != 1 {
 		return nil, errors.New("multiple collections of unassigned nfts found")
