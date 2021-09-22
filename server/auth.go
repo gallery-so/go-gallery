0651fef4eda2a3c341f01114e71d2180c00a0927
bennycio <benny@bennyc.io>
2021-09-22 10:56:57 -0400

    authed preflight no NFT check

--- server/auth.go
+++ server/auth.go
@@ -58,8 +58,10 @@ func getAuthPreflight(pRuntime *runtime.Runtime) gin.HandlerFunc {
 			return
 		}
 
+		authed := c.GetBool(authContextKey)
+
 		// GET_PUBLIC_INFO
-		output, err := authUserGetPreflightDb(c, input, pRuntime)
+		output, err := authUserGetPreflightDb(c, input, authed, pRuntime)
 		if err != nil {
 			// TODO: log specific error and return user friendly error message instead
 			c.JSON(http.StatusInternalServerError, errorResponse{Error: err.Error()})
@@ -291,7 +293,7 @@ func authVerifySignature(pSignatureStr string,
 
 }
 
-func authUserGetPreflightDb(pCtx context.Context, pInput *authUserGetPreflightInput,
+func authUserGetPreflightDb(pCtx context.Context, pInput *authUserGetPreflightInput, pPreAuthed bool,
 	pRuntime *runtime.Runtime) (*authUserGetPreflightOutput, error) {
 
 	user, err := persist.UserGetByAddress(pCtx, pInput.Address, pRuntime)
@@ -304,12 +306,14 @@ func authUserGetPreflightDb(pCtx context.Context, pInput *authUserGetPreflightIn
 	var nonce *persist.UserNonce
 	if !userExistsBool {
 
-		hasNFT, err := hasAnyNFT(pCtx, "0x0", pInput.Address, pRuntime)
-		if err != nil {
-			return nil, err
-		}
-		if !hasNFT {
-			return nil, errors.New("user does not own required nft to signup")
+		if !pPreAuthed {
+			hasNFT, err := hasAnyNFT(pCtx, "0x0", pInput.Address, pRuntime)
+			if err != nil {
+				return nil, err
+			}
+			if !hasNFT {
+				return nil, errors.New("user does not own required nft to signup")
+			}
 		}
 
 		nonce = &persist.UserNonce{
