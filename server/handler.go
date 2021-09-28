63a36d59afaa445ea82409e25dd2e2618881502a
Benny <benny@bennyc.io>
2021-09-28 00:33:59 -0400

    authed preflight no NFT check (#163)
    * authed preflight no NFT check
    
    * disable check for now

--- server/handler.go
+++ server/handler.go
@@ -63,7 +63,7 @@ func authHandlersInit(pRuntime *runtime.Runtime, parent *gin.RouterGroup) {
 	// called before login/sugnup calls, mostly to get nonce and also discover if user exists.
 
 	// [GET] /glry/v1/auth/get_preflight?address=:walletAddress
-	authGroup.GET("/get_preflight", getAuthPreflight(pRuntime))
+	authGroup.GET("/get_preflight", jwtOptional(pRuntime), getAuthPreflight(pRuntime))
 
 	// AUTH VALIDATE_JWT
 
