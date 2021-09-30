9c3d7a2fe51fc8da02cebc16168dcc4e7a54566b
bennycio <benny@bennyc.io>
2021-09-30 11:43:11 -0400

    failure test cases

--- server/t__auth_test.go
+++ server/t__auth_test.go
@@ -21,6 +21,30 @@ func TestAuthVerifySignature_Success(t *testing.T) {
 	assert.True(t, success)
 }
 
+func TestAuthVerifySignature_WrongNonce_Failure(t *testing.T) {
+	setupTest(t)
+
+	testNonce := "Wrong Nonce despite address signing sig"
+	sig := "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b"
+	addr := "0x456d569592f15Af845D0dbe984C12BAB8F430e31"
+
+	success, err := authVerifySignatureAllMethods(sig, testNonce, addr, tc.r)
+	assert.NotNil(t, err)
+	assert.False(t, success)
+}
+
+func TestAuthVerifySignature_WrongAddress_Failure(t *testing.T) {
+	setupTest(t)
+
+	testNonce := "TestNonce"
+	sig := "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b"
+	addr := "0x456d569592f15Af845D0dbe984C12BAB8F430e32"
+
+	success, err := authVerifySignatureAllMethods(sig, testNonce, addr, tc.r)
+	assert.NotNil(t, err)
+	assert.False(t, success)
+}
+
 func TestJwtValid_Success(t *testing.T) {
 	setupTest(t)
 	assert := assert.New(t)
@@ -32,8 +56,20 @@ func TestJwtValid_Success(t *testing.T) {
 	assert.Nil(err)
 	assert.True(output.IsValid)
 	assert.Equal(tc.user1.id, output.UserID)
-
 }
+
+func TestJwtValid_WrongSignatureAndClaims_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+	resp := jwtValidRequest(assert, "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJUZXN0IiwiaWF0IjoxNjMzMDE1MTM1LCJleHAiOjE2NjQ1NTExMzUsImF1ZCI6InRlc3QiLCJzdWIiOiJ0ZXN0IiwiVGVzdCI6IlRlc3QifQ.ewGO4x1xEN01CCZTp5vg0d_rxzdzH_rY0zBXVT1OVJY")
+	assertValidJSONResponse(assert, resp)
+
+	output := &jwtValidateResponse{}
+	err := util.UnmarshallBody(output, resp.Body)
+	assert.Nil(err)
+	assert.False(output.IsValid)
+}
+
 func jwtValidRequest(assert *assert.Assertions, jwt string) *http.Response {
 	req, err := http.NewRequest("GET",
 		fmt.Sprintf("%s/auth/jwt_valid", tc.serverURL),
--- server/t__routes_auth_test.go
+++ server/t__routes_auth_test.go
@@ -32,6 +32,42 @@ func TestAuthPreflightUserExists_Success(t *testing.T) {
 	assert.True(output.UserExists)
 }
 
+func TestAuthPreflightUserNotExists_Success(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	resp := getPreflightRequest(assert, "0x456d569592f15Af845D0dbe984C12BAB8F430e31", "")
+	assertValidResponse(assert, resp)
+
+	type PreflightResp struct {
+		authUserGetPreflightOutput
+		Error string `json:"error"`
+	}
+	output := &PreflightResp{}
+	err := util.UnmarshallBody(output, resp.Body)
+	assert.Nil(err)
+	assert.Empty(output.Error)
+	assert.False(output.UserExists)
+}
+
+func TestAuthPreflightUserNotExistWithJWT_Success(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	resp := getPreflightRequest(assert, "0x456d569592f15Af845D0dbe984C12BAB8F430e31", tc.user1.jwt)
+	assertValidResponse(assert, resp)
+
+	type PreflightResp struct {
+		authUserGetPreflightOutput
+		Error string `json:"error"`
+	}
+	output := &PreflightResp{}
+	err := util.UnmarshallBody(output, resp.Body)
+	assert.Nil(err)
+	assert.Empty(output.Error)
+	assert.False(output.UserExists)
+}
+
 func TestUserCreate_Success(t *testing.T) {
 	setupTest(t)
 	assert := assert.New(t)
@@ -56,6 +92,58 @@ func TestUserCreate_Success(t *testing.T) {
 	assert.Empty(output.Error)
 	assert.True(output.SignatureValid)
 	assert.NotEmpty(output.UserID)
+}
+func TestUserCreate_WrongNonce_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	nonce := &persist.UserNonce{
+		Value:   "Wrong Nonce",
+		Address: strings.ToLower("0x456d569592f15Af845D0dbe984C12BAB8F430e31"),
+	}
+	_, err := persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := createUserRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", nonce.Address)
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserCreate_WrongSig_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	nonce := &persist.UserNonce{
+		Value:   "TestNonce",
+		Address: strings.ToLower("0x456d569592f15Af845D0dbe984C12BAB8F430e31"),
+	}
+	_, err := persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := createUserRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb808s191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", nonce.Address)
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserCreate_WrongAddress_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	nonce := &persist.UserNonce{
+		Value:   "TestNonce",
+		Address: strings.ToLower("0x456d569592f15Af845D0dbe984C12BAB8F430e32"),
+	}
+	_, err := persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := createUserRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", nonce.Address)
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserCreate_NoNonce_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	resp := createUserRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", "0x456d569592f15Af845D0dbe984C12BAB8F430e32")
+	assertErrorResponse(assert, resp)
 }
 
 func TestUserLogin_Success(t *testing.T) {
@@ -92,12 +180,129 @@ func TestUserLogin_Success(t *testing.T) {
 	assert.NotEmpty(output.UserID)
 }
 
+func TestUserLogin_WrongNonce_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	user := &persist.User{
+		Addresses: []string{strings.ToLower("0x456d569592f15Af845D0dbe984C12BAB8F430e31")},
+	}
+
+	userID, err := persist.UserCreate(context.Background(), user, tc.r)
+	assert.Nil(err)
+
+	nonce := &persist.UserNonce{
+		Value:   "Wrong Nonce",
+		UserID:  userID,
+		Address: user.Addresses[0],
+	}
+	_, err = persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := loginRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", nonce.Address)
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserLogin_WrongSig_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	user := &persist.User{
+		Addresses: []string{strings.ToLower("0x456d569592f15Af845D0dbe984C12BAB8F430e31")},
+	}
+
+	userID, err := persist.UserCreate(context.Background(), user, tc.r)
+	assert.Nil(err)
+
+	nonce := &persist.UserNonce{
+		Value:   "TestNonce",
+		UserID:  userID,
+		Address: user.Addresses[0],
+	}
+	_, err = persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := loginRequest(assert, "0x0a22246c5feee38a80dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", nonce.Address)
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserLogin_WrongAddr_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	user := &persist.User{
+		Addresses: []string{strings.ToLower("0x456d569592f15Af845D0dbe984C12BAB8F430e31")},
+	}
+
+	userID, err := persist.UserCreate(context.Background(), user, tc.r)
+	assert.Nil(err)
+
+	nonce := &persist.UserNonce{
+		Value:   "TestNonce",
+		UserID:  userID,
+		Address: user.Addresses[0],
+	}
+	_, err = persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := loginRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", "0xcb1b78568d0Ef81585f074b0Dfd6B743959070D9")
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserLogin_NoNonce_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	user := &persist.User{
+		Addresses: []string{strings.ToLower("0x456d569592f15Af845D0dbe984C12BAB8F430e31")},
+	}
+
+	_, err := persist.UserCreate(context.Background(), user, tc.r)
+	assert.Nil(err)
+
+	resp := loginRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", "0x456d569592f15Af845D0dbe984C12BAB8F430e31")
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserLogin_UserNotExist_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	nonce := &persist.UserNonce{
+		Value:   "TestNonce",
+		Address: "0x456d569592f15Af845D0dbe984C12BAB8F430e31",
+	}
+	_, err := persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := loginRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", nonce.Address)
+	assertErrorResponse(assert, resp)
+}
+
+func TestUserLogin_UserNotOwnAddress_Failure(t *testing.T) {
+	setupTest(t)
+	assert := assert.New(t)
+
+	nonce := &persist.UserNonce{
+		Value:   "TestNonce",
+		UserID:  tc.user1.id,
+		Address: "0x456d569592f15Af845D0dbe984C12BAB8F430e31",
+	}
+	_, err := persist.AuthNonceCreate(context.Background(), nonce, tc.r)
+	assert.Nil(err)
+
+	resp := loginRequest(assert, "0x0a22246c5feee38a90dc6898b453c944e7e7c2f9850218d7c13f3f17f992ea691bb8083191a59ad2c83a5d7f4b41d85df1e693a96b5a251f0a66751b7dc235091b", nonce.Address)
+	assertErrorResponse(assert, resp)
+}
+
 func getPreflightRequest(assert *assert.Assertions, address string, jwt string) *http.Response {
 	req, err := http.NewRequest("GET",
 		fmt.Sprintf("%s/auth/get_preflight?address=%s", tc.serverURL, address),
 		nil)
 	assert.Nil(err)
-	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", jwt))
+	if jwt != "" {
+		req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", jwt))
+	}
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	assert.Nil(err)
