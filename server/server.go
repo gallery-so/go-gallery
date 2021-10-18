b97a30d4ea83fdd4d63599d3f5430737133e25c9
bennycio <benny@bennyc.io>
2021-10-18 11:04:14 -0400

    add middleware to all jwtRequired endpoints

--- server/server.go
+++ server/server.go
@@ -79,7 +79,7 @@ func setDefaults() {
 	viper.SetDefault("GCLOUD_TOKEN_CONTENT_BUCKET", "token-content")
 	viper.SetDefault("REDIS_URL", "localhost:6379")
 	viper.SetDefault("GOOGLE_APPLICATION_CREDENTIALS", "decrypted/service-key.json")
-	viper.SetDefault("CONTRACT_ADDRESS", "0x876e785A1EE39f0655BE10a1440DBde3e53D3F57")
+	viper.SetDefault("CONTRACT_ADDRESS", "0x559A813A56D8039e5E3d9366d46189F4E96fb678")
 	viper.SetDefault("CONTRACT_INTERACTION_URL", "https://eth-rinkeby.alchemyapi.io/v2/lZc9uHY6g2ak1jnEkrOkkopylNJXvE76")
 
 	viper.AutomaticEnv()
