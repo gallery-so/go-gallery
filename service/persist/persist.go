021647e40400cf8436dc3012494088a3833801db
bennycio <benny@bennyc.io>
2021-12-28 12:57:37 -0800

    return correct value type and correct docker file env var

--- service/persist/persist.go
+++ service/persist/persist.go
@@ -39,7 +39,7 @@ func (d *DBID) Scan(i interface{}) error {
 // Value implements the database/sql driver Valuer interface for the DBID type
 func (d DBID) Value() (driver.Value, error) {
 	if d.String() == "" {
-		return GenerateID(), nil
+		return GenerateID().String, nil
 	}
 	return d.String(), nil
 }
