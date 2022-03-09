ff6eb4c6ecf5e45680b85e50442917cb3f62298d
bennycio <benny@bennyc.io>
2022-03-08 19:12:41 -0500

    other communities

--- service/community/community.go
+++ service/community/community.go
@@ -3,6 +3,7 @@ package community
 import (
 	"context"
 	"fmt"
+	"math/big"
 
 	"github.com/mikeydub/go-gallery/service/nft"
 	"github.com/mikeydub/go-gallery/service/persist"
@@ -25,6 +26,33 @@ var communities = map[persist.Address]communityData{
 		Description:     "it's the season of the witch. 🌙",
 		BannerImageURL:  "https://lh3.googleusercontent.com/M42Xf9Vbu_yodzKVFA1I6TYXIx5Hz699gEtp2lDg9vGT7g-S4z_5cx2iYPub1kytnOlexV5WDdGOmpGeuH4-N0CYXi7FaC_iqEm4gQ=h600",
 		ProfileImageURL: "https://lh3.googleusercontent.com/E8MVasG7noxC0Fa_duhnexc2xze1PzT1jzyeaHsytOC4722C2Zeo7EhUR8-T6mSem9-4XE5ylrCtoAsceZ_lXez_kTaMufV5pfLc3Fk=s130",
+	},
+	// poolsuite exec
+	"0xb228d7b6e099618ca71bd5522b3a8c3788a8f172": {
+		Name:            "Poolsuite - Executive Member",
+		ProfileImageURL: "https://lh3.googleusercontent.com/p2lvRuQZalsroxpmS-q57pGRzyseAzEkLOGGsR6N6tXh_d4x6osxQtZBKqUMRreepnXJcuR80d-9YRIeMy5XnEsPp8aQQcWOyyPqjg=s130",
+		BannerImageURL:  "https://lh3.googleusercontent.com/pN8dsrbR5TnTRCtTD7qNqZABzny1JB1tx5xXSSz3XdlovlptmEymNcQ7JE1DZJTZZHEqDg2dzArvcucoUL0dDHdnAMLxU_Nf-gsFSQ=h600",
+	},
+	// poolsuite member
+	"0x123214ef2bb526d1b3fb84a6d448985f537d9763": {
+		Name:            "Poolsuite - Pool Member",
+		ProfileImageURL: "https://lh3.googleusercontent.com/y8b77O5wH-7pd2jK2eRBDkkJV_SO56FkCPqx3_L6UVdKLFXx1IxqUUYoGSh0m-2jQhyzS4TKdmFPNnsBZlVRQ6rxjLVPaLUOjQS-Gfk=s130",
+		BannerImageURL:  "https://lh3.googleusercontent.com/S-Zjma4LRM3_Krpb_Uu_lPEStcOQ5vgolc3yqfafCOzc3o6O1ZKRCmdKwN2Dpg5cIJ54XFP639D11wOQT7dx6rADnsb1AR9f8PNi=h600",
+	},
+	// poolsuite patron
+	"0x364547adfc7a180744d762056df35eeaea803ec4": {
+		Name:            "Poolsuite - Patron of the Pool",
+		ProfileImageURL: "https://lh3.googleusercontent.com/9U-ga_5kztUukT_6bpEBtyFVbmTM9ywObB7MDbB_9iwnY9ML1cpCh63yeU2H2Q43thMlGuGhNAOZDrpN8ATAQU1Y9O_Ffqprvk-U=s130",
+		BannerImageURL:  "https://lh3.googleusercontent.com/MAkRl41geA5QaqEk4EYBEazEUkVZyvzNOQSn63QcX4kKMDKqj-okb23UtF6q5Mw0Ry_bHUyPb_nzuHj-PjRCHE2tT_PEE8Od0O9S=h600",
+	},
+	"0x13aae6f9599880edbb7d144bb13f1212cee99533": {
+		Name:        "The Leggendas",
+		Description: "Future pasts, lost civilisations. A reminiscence of the new renaissance. What will remain of us, at the end of time? Who will stand, to tell our story?",
+		TokenIDRanges: []persist.TokenIDRange{
+			{Start: persist.TokenID(big.NewInt(1000000).Text(16)), End: persist.TokenID(big.NewInt(1000887).Text(16))},
+		},
+		BannerImageURL:  "https://lh3.googleusercontent.com/l4j8nhI86Jj80fBrhnvHXksaXBr4Ou4yW4mvoqlzC2fsSi-MlE1mErg8mf5PGiUQjUufq2A9A3Mheg0eSnu8i_wxoLP8gVzgmcpMXaw=h600",
+		ProfileImageURL: "https://lh3.googleusercontent.com/ParWg5Mn9HltQgoBapkIDzmeG4N2yIYtwdnQuibiscX8FIjFc8bfOeAl7Qt3ccrkU1BAUjeGqyMW3AeKbNMvcQEVET2tIME6fi0-ns8=w192",
 	},
 }
 
@@ -68,9 +96,13 @@ func UpdateCommunity(pCtx context.Context, addr persist.Address, nftRepository p
 	}
 	allNFTs = filterValidRanges(data.TokenIDRanges, allNFTs)
 
+	seenUsers := make(map[persist.DBID]bool)
 	for _, anN := range allNFTs {
 		user, err := userRepository.GetByAddress(pCtx, anN.OwnerAddress)
 		if err != nil {
+			continue
+		}
+		if seenUsers[user.ID] {
 			continue
 		}
 		galleries, err := galleryRepository.GetByUserID(pCtx, user.ID)
@@ -95,6 +127,7 @@ func UpdateCommunity(pCtx context.Context, addr persist.Address, nftRepository p
 				}
 			}
 		}
+		seenUsers[user.ID] = true
 	}
 	community.Owners = owners
 	err = communityRepository.UpsertByContract(pCtx, addr, community)
