ea560dd99247ac5070b81934aaf82b081995f605
Robinnnnn <rokim8@gmail.com>
2022-03-28 20:41:30 -0400

    add test to ensure backups appear if past 5 mins since previous

--- service/persist/postgres/t__backup_test.go
+++ service/persist/postgres/t__backup_test.go
@@ -3,6 +3,7 @@ package postgres
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/mikeydub/go-gallery/service/memstore/redis"
 	"github.com/mikeydub/go-gallery/service/persist"
@@ -107,7 +108,8 @@ func TestBackupRestore_Success(t *testing.T) {
 	a.Len(galleries[0].Collections, 1)
 }
 
-func TestOneBackupADay_Success(t *testing.T) {
+// backups made within 5 mins of each other should be de-duped
+func TestThrottle_Success(t *testing.T) {
 	a, db := setupTest(t)
 
 	galleryRepo := NewGalleryRepository(db, redis.NewCache(0))
@@ -117,7 +119,6 @@ func TestOneBackupADay_Success(t *testing.T) {
 	backupRepo := NewBackupRepository(db)
 
 	user := persist.User{
-
 		Username:           "username",
 		UsernameIdempotent: "username-idempotent",
 		Addresses: []persist.Address{
@@ -192,5 +193,92 @@ func TestOneBackupADay_Success(t *testing.T) {
 	backups, err = backupRepo.Get(context.Background(), userID)
 	a.NoError(err)
 	a.Len(backups, 1)
+	a.Len(backups[0].Gallery.Collections, 1)
+}
+
+// backups made over 5 mins of each other should be added
+func TestNoThrottle_Success(t *testing.T) {
+	a, db := setupTest(t)
+
+	galleryRepo := NewGalleryRepository(db, redis.NewCache(0))
+	collectionRepo := NewCollectionRepository(db, galleryRepo)
+	nftRepo := NewNFTRepository(db, galleryRepo)
+	userRepo := NewUserRepository(db)
+	backupRepo := NewBackupRepository(db)
+
+	user := persist.User{
+		Username:           "username",
+		UsernameIdempotent: "username-idempotent",
+		Addresses: []persist.Address{
+			"0x8914496dc01efcc49a2fa340331fb90969b6f1d2",
+		},
+	}
+
+	userID, err := userRepo.Create(context.Background(), user)
+	a.NoError(err)
+
+	nfts := []persist.NFT{
+		{
+			OwnerAddress: "0x8914496dc01efcc49a2fa340331fb90969b6f1d2",
+			Name:         "name",
+			OpenseaID:    1,
+		},
+		{
+			OwnerAddress: "0x8914496dc01efcc49a2fa340331fb90969b6f1d2",
+			Name:         "blah blah",
+			OpenseaID:    10,
+		},
+	}
+
+	ids, err := nftRepo.CreateBulk(context.Background(), nfts)
+	a.NoError(err)
+
+	collection := persist.CollectionDB{
+		Name:        "name",
+		OwnerUserID: userID,
+		NFTs:        ids,
+	}
+
+	collID, err := collectionRepo.Create(context.Background(), collection)
+	a.NoError(err)
+
+	gallery := persist.GalleryDB{
+		OwnerUserID: userID,
+		Collections: []persist.DBID{collID},
+	}
+
+	id, err := galleryRepo.Create(context.Background(), gallery)
+	a.NoError(err)
+
+	g, err := galleryRepo.GetByID(context.Background(), id)
+	a.NoError(err)
+
+	err = backupRepo.Insert(context.Background(), g)
+	a.NoError(err)
+
+	backups, err := backupRepo.Get(context.Background(), userID)
+	a.NoError(err)
+	a.Len(backups, 1)
+	a.Len(backups[0].Gallery.Collections, 1)
+
+	collection2 := persist.CollectionDB{
+		Name:        "name2",
+		OwnerUserID: userID,
+		NFTs:        ids,
+	}
+
+	collID2, err := collectionRepo.Create(context.Background(), collection2)
+	a.NoError(err)
+
+	err = galleryRepo.Update(context.Background(), id, userID, persist.GalleryUpdateInput{
+		Collections: []persist.DBID{collID, collID2},
+	})
+	a.NoError(err)
+
+	backupRepo.insertBackupStmt.ExecContext(context.Background(), persist.GenerateID(), g.ID, g.Version, g, persist.CreationTime(time.Now().Local().Add(time.Hour)))
+
+	backups, err = backupRepo.Get(context.Background(), userID)
+	a.NoError(err)
+	a.Len(backups, 2)
 	a.Len(backups[0].Gallery.Collections, 1)
 }
