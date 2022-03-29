dcaf2cf17fa975e8be24181c376594d6f574b9ad
Robinnnnn <rokim8@gmail.com>
2022-03-29 00:49:44 -0400

    mock gallery creation helper

--- service/persist/postgres/t__utils_test.go
+++ service/persist/postgres/t__utils_test.go
@@ -1,9 +1,12 @@
 package postgres
 
 import (
+	"context"
 	"database/sql"
 	"testing"
 
+	"github.com/mikeydub/go-gallery/service/memstore/redis"
+	"github.com/mikeydub/go-gallery/service/persist"
 	"github.com/spf13/viper"
 	"github.com/stretchr/testify/assert"
 )
@@ -28,4 +31,61 @@ func setupTest(t *testing.T) (*assert.Assertions, *sql.DB) {
 	})
 
 	return assert.New(t), db
+}
+
+func createMockGallery(t *testing.T, a *assert.Assertions, db *sql.DB) (persist.DBID, persist.DBID, []persist.DBID, persist.Gallery, *BackupRepository, *GalleryRepository, *CollectionRepository, *NFTRepository, *UserRepository) {
+	galleryRepo := NewGalleryRepository(db, redis.NewCache(0))
+	collectionRepo := NewCollectionRepository(db, galleryRepo)
+	nftRepo := NewNFTRepository(db, galleryRepo)
+	userRepo := NewUserRepository(db)
+	backupRepo := NewBackupRepository(db)
+
+	user := persist.User{
+		Username:           "username",
+		UsernameIdempotent: "username-idempotent",
+		Addresses: []persist.Address{
+			"0x8914496dc01efcc49a2fa340331fb90969b6f1d2",
+		},
+	}
+
+	userID, err := userRepo.Create(context.Background(), user)
+	a.NoError(err)
+
+	nfts := []persist.NFT{
+		{
+			OwnerAddress: "0x8914496dc01efcc49a2fa340331fb90969b6f1d2",
+			Name:         "name",
+			OpenseaID:    1,
+		},
+		{
+			OwnerAddress: "0x8914496dc01efcc49a2fa340331fb90969b6f1d2",
+			Name:         "blah blah",
+			OpenseaID:    10,
+		},
+	}
+
+	nftIDs, err := nftRepo.CreateBulk(context.Background(), nfts)
+	a.NoError(err)
+
+	collection := persist.CollectionDB{
+		Name:        "name",
+		OwnerUserID: userID,
+		NFTs:        nftIDs,
+	}
+
+	collID, err := collectionRepo.Create(context.Background(), collection)
+	a.NoError(err)
+
+	gallery := persist.GalleryDB{
+		OwnerUserID: userID,
+		Collections: []persist.DBID{collID},
+	}
+
+	id, err := galleryRepo.Create(context.Background(), gallery)
+	a.NoError(err)
+
+	g, err := galleryRepo.GetByID(context.Background(), id)
+	a.NoError(err)
+
+	return userID, collID, nftIDs, g, backupRepo, galleryRepo, collectionRepo, nftRepo, userRepo
 }
