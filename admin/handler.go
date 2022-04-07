21ca15b852cf8c21ae6e1c7af19354fec9a5c6b6
Benny <benny@bennyc.io>
2022-04-07 13:36:11 -0400

    address user admin (#337)

--- admin/handler.go
+++ admin/handler.go
@@ -12,7 +12,7 @@ func handlersInit(router *gin.Engine, db *sql.DB, stmts *statements, ethcl *ethc
 	api := router.Group("/admin/v1")
 
 	users := api.Group("/users")
-	users.GET("/get", getUser(stmts.getUserByIDStmt, stmts.getUserByUsernameStmt))
+	users.GET("/get", getUser(stmts.getUserByIDStmt, stmts.getUserByUsernameStmt, stmts.getUserByAddressStmt))
 	users.POST("/merge", mergeUser(db, stmts.getUserByIDStmt, stmts.updateUserStmt, stmts.deleteUserStmt, stmts.getGalleriesRawStmt, stmts.deleteGalleryStmt, stmts.updateGalleryStmt))
 	users.POST("/update", updateUser(stmts.updateUserStmt))
 	users.POST("/delete", deleteUser(db, stmts.deleteUserStmt, stmts.getGalleriesRawStmt, stmts.deleteGalleryStmt, stmts.deleteCollectionStmt))
