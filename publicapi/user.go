f3c887c4986c2a8b2499ada805da1e4634971b92
Ezra Smith <ezrasmith@gmail.com>
2022-04-11 14:50:21 -0400

    Don't validate user bio with the "required" tag

--- publicapi/user.go
+++ publicapi/user.go
@@ -127,7 +127,7 @@ func (api UserAPI) UpdateUserInfo(ctx context.Context, username string, bio stri
 	// Validate
 	if err := validateFields(api.validator, validationMap{
 		"username": {username, "required,username"},
-		"bio":      {bio, "required,bio"},
+		"bio":      {bio, "bio"},
 	}); err != nil {
 		return err
 	}
