4168f442483b8a3c4dd25addaf12fb82d4045ef4
Ezra Smith <ezrasmith@gmail.com>
2022-04-13 17:10:16 -0400

    Removed unneeded repos/ethClient fields from Resolver struct

--- graphql/resolver/resolver.go
+++ graphql/resolver/resolver.go
@@ -1,10 +1,5 @@
 package graphql
 
-import (
-	"github.com/ethereum/go-ethereum/ethclient"
-	"github.com/mikeydub/go-gallery/service/persist"
-)
-
 // Add gqlgen to "go generate"
 //go:generate go run generate.go
 
@@ -12,7 +7,4 @@ import (
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct {
-	Repos     *persist.Repositories
-	EthClient *ethclient.Client
-}
+type Resolver struct{}
