ebe4585bcc63fd7468dbd91a496065d5dfbdc6e7
jarrel-b <jarrel.biscocho@gmail.com>
2022-05-28 17:21:55 -0700

    Add poster

--- feedbot/post.go
+++ feedbot/post.go
@@ -12,11 +12,19 @@ import (
 type Post struct {
 	name     string
 	criteria []func(Query) bool
-	renderer func(Query) string
+	poster   Poster
 }
 
-func (p *Post) Handle(ctx context.Context, q Query) (bool, error) {
-	content := p.renderer(q)
+type Poster interface {
+	Send(context.Context, Query) error
+}
+
+type DiscordPoster struct {
+	render func(Query) string
+}
+
+func (d *DiscordPoster) Send(ctx context.Context, q Query) error {
+	content := d.render(q)
 
 	message, err := json.Marshal(map[string]interface{}{
 		"content": content,
@@ -24,13 +32,16 @@ func (p *Post) Handle(ctx context.Context, q Query) (bool, error) {
 	})
 
 	if err != nil {
-		return false, err
+		return err
 	}
 
-	if err := sendMessage(ctx, message); err != nil {
+	return sendMessage(ctx, message)
+}
+
+func (p *Post) Handle(ctx context.Context, q Query) (bool, error) {
+	if err := p.poster.Send(ctx, q); err != nil {
 		return false, err
 	}
-
 	return true, nil
 }
 
@@ -87,120 +98,154 @@ func nftURL(username, collectionID, nftID string) string {
 }
 
 var userCreatedPost = Post{
-	name:     "UserCreated",
-	criteria: append(baseCriteria, isUserFollowedEvent, userNoEventsBefore),
-	renderer: func(q Query) string {
-		return fmt.Sprintf("**%s** joined Gallery: %s", q.Username, userURL(q.Username))
+	name: "UserCreated",
+	criteria: append(
+		baseCriteria,
+		isUserFollowedEvent,
+		userNoEventsBefore,
+	),
+	poster: &DiscordPoster{
+		render: func(q Query) string {
+			return fmt.Sprintf("**%s** joined Gallery: %s", q.Username, userURL(q.Username))
+		},
 	},
 }
 
 var userFollowedPost = Post{
 	name:     "UserFollowed",
 	criteria: append(baseCriteria, isUserFollowedEvent, followedUserHasUsername),
-	renderer: func(q Query) string {
-		return fmt.Sprintf("**%s** followed **%s**: %s", q.Username, q.FollowedUsername, userURL(q.FollowedUsername))
+	poster: &DiscordPoster{
+		render: func(q Query) string {
+			return fmt.Sprintf("**%s** followed **%s**: %s", q.Username, q.FollowedUsername, userURL(q.FollowedUsername))
+		},
 	},
 }
 
 var nftCollectorsNoteAddedPost = Post{
-	name:     "CollectorsNoteAdded",
-	criteria: append(baseCriteria, isNftCollectorsNoteAddedEvent, nftHasCollectorsNote, nftBelongsToCollection),
-	renderer: func(q Query) string {
-		var message string
-		if q.NftName != "" {
-			message = fmt.Sprintf("**%s** added a collector's note to *%s*: %s", q.Username, q.NftName, nftURL(q.Username, q.CollectionID.String(), q.NftID.String()))
-		} else {
-			message = fmt.Sprintf("**%s** added a collector's note to their NFT: %s", q.Username, nftURL(q.Username, q.CollectionID.String(), q.NftID.String()))
-		}
-		return message
+	name: "CollectorsNoteAdded",
+	criteria: append(
+		baseCriteria,
+		isNftCollectorsNoteAddedEvent,
+		nftHasCollectorsNote,
+		nftBelongsToCollection,
+	),
+	poster: &DiscordPoster{
+		render: func(q Query) string {
+			var message string
+			if q.NftName != "" {
+				message = fmt.Sprintf("**%s** added a collector's note to *%s*: %s", q.Username, q.NftName, nftURL(q.Username, q.CollectionID.String(), q.NftID.String()))
+			} else {
+				message = fmt.Sprintf("**%s** added a collector's note to their NFT: %s", q.Username, nftURL(q.Username, q.CollectionID.String(), q.NftID.String()))
+			}
+			return message
+		},
 	},
 }
 
 var collectionCreatedPost = Post{
-	name:     "CollectionCreated",
-	criteria: append(baseCriteria, isCollectionCreatedEvent, collectionHasNfts),
-	renderer: func(q Query) string {
-		var message string
-		if q.CollectionName != "" {
-			message = fmt.Sprintf("**%s** created a collection titled '*%s'*: %s", q.Username, q.CollectionName, collectionURL(q.Username, q.CollectionID.String()))
-		} else {
-			message = fmt.Sprintf("**%s** created a collection: %s", q.Username, collectionURL(q.Username, q.CollectionID.String()))
-		}
-		return message
+	name: "CollectionCreated",
+	criteria: append(
+		baseCriteria,
+		isCollectionCreatedEvent,
+		collectionHasNfts,
+	),
+	poster: &DiscordPoster{
+		render: func(q Query) string {
+			var message string
+			if q.CollectionName != "" {
+				message = fmt.Sprintf("**%s** created a collection titled '*%s'*: %s", q.Username, q.CollectionName, collectionURL(q.Username, q.CollectionID.String()))
+			} else {
+				message = fmt.Sprintf("**%s** created a collection: %s", q.Username, collectionURL(q.Username, q.CollectionID.String()))
+			}
+			return message
+		},
 	},
 }
 
 var collectionCollectorsNoteAddedPost = Post{
-	name:     "CollectionCollectorsNoteAdded",
-	criteria: append(baseCriteria, isCollectionCollectorsNoteAddedEvent, collectionHasCollectorsNote),
-	renderer: func(q Query) string {
-		var message string
-		if q.CollectionName != "" {
-			message = fmt.Sprintf("**%s** added a collector's note to their collection, *%s*: %s", q.Username, q.CollectionName, collectionURL(q.Username, q.CollectionID.String()))
-		} else {
-			message = fmt.Sprintf("**%s** added a collector's note to their collection: %s", q.Username, collectionURL(q.Username, q.CollectionID.String()))
-		}
-		return message
+	name: "CollectionCollectorsNoteAdded",
+	criteria: append(
+		baseCriteria,
+		isCollectionCollectorsNoteAddedEvent,
+		collectionHasCollectorsNote,
+	),
+	poster: &DiscordPoster{
+		render: func(q Query) string {
+			var message string
+			if q.CollectionName != "" {
+				message = fmt.Sprintf("**%s** added a collector's note to their collection, *%s*: %s", q.Username, q.CollectionName, collectionURL(q.Username, q.CollectionID.String()))
+			} else {
+				message = fmt.Sprintf("**%s** added a collector's note to their collection: %s", q.Username, collectionURL(q.Username, q.CollectionID.String()))
+			}
+			return message
+		},
 	},
 }
 
 var collectionTokensAddedPost = Post{
-	name:     "CollectionTokensAdded",
-	criteria: append(baseCriteria, isCollectionTokensAddedEvent, collectionHasNfts, collectionHasNewTokensAdded),
-	renderer: func(q Query) string {
-		var tokensAdded int
-		var tokenName string
+	name: "CollectionTokensAdded",
+	criteria: append(
+		baseCriteria,
+		isCollectionTokensAddedEvent,
+		collectionHasNfts,
+		collectionHasNewTokensAdded,
+	),
+	poster: &DiscordPoster{
+		render: func(q Query) string {
+			var tokensAdded int
+			var tokenName string
 
-		if q.LastCollectionEvent != nil {
-			for _, nft := range q.CollectionNfts {
-				contains := false
-				for _, otherId := range q.LastCollectionEvent.Data.NFTs {
-					if nft.Nft.Dbid == otherId {
-						contains = true
+			if q.LastCollectionEvent != nil {
+				for _, nft := range q.CollectionNfts {
+					contains := false
+					for _, otherId := range q.LastCollectionEvent.Data.NFTs {
+						if nft.Nft.Dbid == otherId {
+							contains = true
+							break
+						}
+					}
+					if !contains {
+						tokensAdded++
+						if tokenName == "" && nft.Nft.Name != "" {
+							tokenName = nft.Nft.Name
+						}
+					}
+				}
+			} else {
+				tokensAdded = len(q.CollectionNfts)
+
+				for _, nft := range q.CollectionNfts {
+					if nft.Nft.Name != "" {
+						tokenName = nft.Nft.Name
 						break
 					}
 				}
-				if !contains {
-					tokensAdded++
-					if tokenName == "" && nft.Nft.Name != "" {
-						tokenName = nft.Nft.Name
-					}
+			}
+
+			url := collectionURL(q.Username, q.CollectionID.String())
+			var message string
+
+			if q.CollectionName != "" && tokenName != "" {
+				message = fmt.Sprintf("**%s** added *%s* ", q.Username, tokenName)
+				if tokensAdded == 1 {
+					message += fmt.Sprintf("to their collection, *%s*: %s", q.CollectionName, url)
+				} else {
+					message += fmt.Sprintf("and %v other NFT(s) to their collection, *%s*: %s", tokensAdded-1, q.CollectionName, url)
 				}
-			}
-		} else {
-			tokensAdded = len(q.CollectionNfts)
-
-			for _, nft := range q.CollectionNfts {
-				if nft.Nft.Name != "" {
-					tokenName = nft.Nft.Name
-					break
+			} else if q.CollectionName == "" && tokenName != "" {
+				message = fmt.Sprintf("**%s** added *%s* ", q.Username, tokenName)
+				if tokensAdded == 1 {
+					message += fmt.Sprintf("to their collection: %s", url)
+				} else {
+					message += fmt.Sprintf("and %v other NFT(s) to their collection: %s", tokensAdded-1, url)
 				}
-			}
-		}
-
-		url := collectionURL(q.Username, q.CollectionID.String())
-		var message string
-
-		if q.CollectionName != "" && tokenName != "" {
-			message = fmt.Sprintf("**%s** added *%s* ", q.Username, tokenName)
-			if tokensAdded == 1 {
-				message += fmt.Sprintf("to their collection, *%s*: %s", q.CollectionName, url)
+			} else if q.CollectionName != "" && tokenName == "" {
+				message = fmt.Sprintf("**%s** added %v NFT(s) to their collection, *%s*: %s", q.Username, tokensAdded, q.CollectionName, url)
 			} else {
-				message += fmt.Sprintf("and %v other NFT(s) to their collection, *%s*: %s", tokensAdded-1, q.CollectionName, url)
+				message = fmt.Sprintf("**%s** added %v NFT(s) to their collection: %s", q.Username, tokensAdded, url)
 			}
-		} else if q.CollectionName == "" && tokenName != "" {
-			message = fmt.Sprintf("**%s** added *%s* ", q.Username, tokenName)
-			if tokensAdded == 1 {
-				message += fmt.Sprintf("to their collection: %s", url)
-			} else {
-				message += fmt.Sprintf("and %v other NFT(s) to their collection: %s", tokensAdded-1, url)
-			}
-		} else if q.CollectionName != "" && tokenName == "" {
-			message = fmt.Sprintf("**%s** added %v NFT(s) to their collection, *%s*: %s", q.Username, tokensAdded, q.CollectionName, url)
-		} else {
-			message = fmt.Sprintf("**%s** added %v NFT(s) to their collection: %s", q.Username, tokensAdded, url)
-		}
 
-		return message
+			return message
+		},
 	},
 }
