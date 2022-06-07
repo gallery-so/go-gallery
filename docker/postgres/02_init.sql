8822ac287a03d20f4acad4408432ef4f4ae971a3
bennycio <benny@bennyc.io>
2022-06-06 17:21:42 -0700

    unique when not deleted

--- docker/postgres/02_init.sql
+++ docker/postgres/02_init.sql
@@ -88,7 +88,7 @@ CREATE TABLE IF NOT EXISTS tokens (
     BLOCK_NUMBER bigint
 );
 
-CREATE UNIQUE INDEX IF NOT EXISTS token_id_contract_address_chain_owner_user_id_idx ON tokens (TOKEN_ID, CONTRACT_ADDRESS, CHAIN, OWNER_USER_ID);
+CREATE UNIQUE INDEX IF NOT EXISTS token_id_contract_address_chain_owner_user_id_idx ON tokens (TOKEN_ID, CONTRACT_ADDRESS, CHAIN, OWNER_USER_ID) WHERE DELETED = false;
 
 CREATE INDEX IF NOT EXISTS token_id_contract_address_chain_idx ON tokens (TOKEN_ID, CONTRACT_ADDRESS, CHAIN);
 
