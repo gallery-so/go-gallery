badfcd15b49bce5e06b20bc6bb8b8b9a8c3b7bd9
bennycio <benny@bennyc.io>
2022-06-06 19:05:39 -0700

    drop correctly *facepalm*

--- db/migrations/000013_chain_in_index.down.sql
+++ db/migrations/000013_chain_in_index.down.sql
@@ -2,6 +2,6 @@ CREATE UNIQUE INDEX IF NOT EXISTS token_id_contract_address_owner_user_id_idx ON
 
 CREATE INDEX IF NOT EXISTS token_id_contract_address_idx ON tokens (TOKEN_ID, CONTRACT_ADDRESS);
 
-DROP INDEX IF EXISTS token_id_contract_address_chain_owner_user_id_idx ON tokens (TOKEN_ID, CONTRACT_ADDRESS, CHAIN,OWNER_USER_ID);
+DROP INDEX IF EXISTS token_id_contract_address_chain_owner_user_id_idx;
 
-DROP INDEX IF EXISTS token_id_contract_address_chain_idx ON tokens (TOKEN_ID, CONTRACT_ADDRESS, CHAIN);
+DROP INDEX IF EXISTS token_id_contract_address_chain_idx;
