0713cb48c3f70cb0089c4e4fb94ab13e1865ed26
jarrel-b <jarrel.biscocho@gmail.com>
2022-07-08 09:43:00 -0700

    Remove redundant index

--- db/migrations/000020_add_event_time_action_index_to_feed_events.down.sql
+++ db/migrations/000020_add_event_time_action_index_to_feed_events.down.sql
@@ -1,1 +1,2 @@
 DROP INDEX IF EXISTS feed_events_event_time_action_idx;
+CREATE INDEX IF NOT EXISTS feeds_event_timestamp_idx ON feed_events (event_time);
--- db/migrations/000020_add_event_time_action_index_to_feed_events.up.sql
+++ db/migrations/000020_add_event_time_action_index_to_feed_events.up.sql
@@ -1,1 +1,2 @@
 CREATE INDEX IF NOT EXISTS feed_events_event_time_action_idx ON feed_events (event_time, action);
+DROP INDEX IF EXISTS feeds_event_timestamp_idx;
