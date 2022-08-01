c302d26f87e6c23835c68f7fedb395335ecc800b
jarrel-b <jarrel.biscocho@gmail.com>
2022-08-01 14:27:48 -0700

    Add is_spam migration

--- db/migrations/core/000023_add_is_spam_to_tokens.down.sql
+++ db/migrations/core/000023_add_is_spam_to_tokens.down.sql
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 ALTER TABLE tokens DROP COLUMN IF EXISTS is_user_marked_spam;
 ALTER TABLE tokens DROP COLUMN IF EXISTS is_provider_marked_spam;
+=======
+ALTER TABLE tokens DROP COLUMN IF EXISTS is_spam;
+>>>>>>> d195ac2 (Add is_spam migration)
