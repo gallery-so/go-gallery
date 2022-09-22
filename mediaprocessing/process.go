0833e775f42d303d87b9ba75e5ae7cba9df00159
Benny <benny@bennyc.io>
2022-09-22 13:58:13 -0700

    keep alive (#529)
    * keep alive
    
    * run keepalive
    
    * self host

--- mediaprocessing/process.go
+++ mediaprocessing/process.go
@@ -43,11 +43,15 @@ func processIPFSMetadata(queue chan<- ProcessMediaInput, throttler *throttle.Loc
 }
 
 func processMedias(queue <-chan ProcessMediaInput, tokenRepo persist.TokenGalleryRepository, ipfsClient *shell.Shell, arweaveClient *goar.Client, stg *storage.Client, tokenBucket string, throttler *throttle.Locker) {
-	wp := workerpool.New(10)
+	wp := workerpool.New(20)
 	for processInput := range queue {
 		in := processInput
 		logger.For(nil).Infof("Processing Media: %s", in.Key)
 		wp.Submit(func() {
+			done := make(chan struct{})
+
+			go keepAliveUntilDone(done, in.Key)
+
 			innerWp := workerpool.New(10)
 			for _, token := range in.Tokens {
 				t := token
@@ -79,6 +83,7 @@ func processMedias(queue <-chan ProcessMediaInput, tokenRepo persist.TokenGaller
 				})
 			}
 			func() {
+				defer close(done)
 				defer throttler.Unlock(context.Background(), in.Key)
 				innerWp.StopWait()
 				logger.For(nil).Infof("Processing Media: %s - Finished", in.Key)
