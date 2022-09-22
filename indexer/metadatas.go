fc0025d92a0bb7ab98332b0880719cb392a4c67c
jarrel-b <jarrel.biscocho@gmail.com>
2022-09-22 14:29:00 -0700

    Add tests

--- indexer/metadatas.go
+++ indexer/metadatas.go
@@ -333,9 +333,9 @@ type graphResponse struct {
 }
 
 func ens(ctx context.Context, turi persist.TokenURI, addr persist.EthereumAddress, tid persist.TokenID, ethCl *ethclient.Client, ipfs *shell.Shell, arweave *goar.Client) (persist.TokenURI, persist.TokenMetadata, error) {
-	// The TokenID type strips leading zeros when variables are casted to it, but we want the zeros
-	// because its part of the hash that's used to look up a label.
-	// We convert the token ID to decimal then back to hexadecimal to get back the padding.
+	// The TokenID type removes leading zeros, but we want the zeros for ENS because the token ID
+	// is a hash that is used to look up a label. Here, we convert the token ID to decimal then back to
+	// hexadecimal to get back the padding.
 	labelHash := common.BigToHash(tid.BigInt()).Hex()[2:]
 
 	gql := fmt.Sprintf(`
