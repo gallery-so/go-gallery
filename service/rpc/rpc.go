ab1422b9203cfa5edca3f4fe95501e85c7e85491
jarrel <jarrel.biscocho@gmail.com>
2022-09-22 17:16:57 -0700

    Change log level to WARN (#531)

--- service/rpc/rpc.go
+++ service/rpc/rpc.go
@@ -427,7 +427,7 @@ func removeBOM(bs []byte) []byte {
 func GetIPFSData(pCtx context.Context, ipfsClient *shell.Shell, path string) ([]byte, error) {
 	dataReader, err := ipfsClient.Cat(path)
 	if err != nil {
-		logger.For(pCtx).WithError(err).Errorf("error getting cat data from ipfs: %s", path)
+		logger.For(pCtx).WithError(err).Warnf("error getting cat data from ipfs: %s", path)
 
 		url := fmt.Sprintf("%s/ipfs/%s", viper.GetString("IPFS_URL"), path)
 
