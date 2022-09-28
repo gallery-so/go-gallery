f7495bf20325f0b211ce6b5aafdb77f6693c6676
bennycio <benny@bennyc.io>
2022-09-27 17:55:42 -0700

    no keepalive

--- mediaprocessing/handler.go
+++ mediaprocessing/handler.go
@@ -10,8 +10,6 @@ import (
 )
 
 func handlersInitServer(router *gin.Engine, queue chan<- ProcessMediaInput, tokenRepo persist.TokenGalleryRepository, contractRepo persist.ContractGalleryRepository, ipfsClient *shell.Shell, arweaveClient *goar.Client, stg *storage.Client, tokenBucket string, throttler *throttle.Locker) *gin.Engine {
-	router.GET("/keepalive", keepAlive())
-
 	router.POST("/process", processMediaForUsersTokensOfChain(queue, tokenRepo, contractRepo, ipfsClient, arweaveClient, stg, tokenBucket, throttler))
 	return router
 }
