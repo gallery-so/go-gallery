809940025609289623fb558e8872cd91e382a2e5
Ezra Smith <ezrasmith@gmail.com>
2022-10-12 11:14:50 -0400

    Drop DESC from feed_event_id in indexes
    They'll still be ordered (and will default to ASC), but putting DESC there implied that the ordering matters, and it doesn't actually matter on feed_event_id.

--- db/migrations/core/000028_admire_comment_pagination_indexes.up.sql
+++ db/migrations/core/000028_admire_comment_pagination_indexes.up.sql
@@ -1,2 +1,2 @@
-CREATE UNIQUE INDEX IF NOT EXISTS admires_created_at_id_feed_event_id_idx ON admires (created_at desc, id desc, feed_event_id desc) WHERE deleted = false;
-CREATE UNIQUE INDEX IF NOT EXISTS comments_created_at_id_feed_event_id_idx ON comments (created_at desc, id desc, feed_event_id desc) WHERE deleted = false;
+CREATE UNIQUE INDEX IF NOT EXISTS admires_created_at_id_feed_event_id_idx ON admires (created_at desc, id desc, feed_event_id) WHERE deleted = false;
+CREATE UNIQUE INDEX IF NOT EXISTS comments_created_at_id_feed_event_id_idx ON comments (created_at desc, id desc, feed_event_id) WHERE deleted = false;
