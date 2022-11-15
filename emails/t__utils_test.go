d6b0ad8673d9aaacf764aab77f80208df592b2c9
bennycio <benny@bennyc.io>
2022-11-15 10:40:38 -0800

    use addresses

--- emails/t__utils_test.go
+++ emails/t__utils_test.go
@@ -90,13 +90,13 @@ func newRepos(pq *sql.DB, pgx *pgxpool.Pool) *postgres.Repositories {
 func seedNotifications(ctx context.Context, t *testing.T, q *coredb.Queries, repos *postgres.Repositories) {
 
 	email := testUser.Email.String()
-	userID, err := repos.UserRepository.Create(ctx, persist.CreateUserInput{Username: testUser.Username.String, Email: &email})
+	userID, err := repos.UserRepository.Create(ctx, persist.CreateUserInput{Username: testUser.Username.String, Email: &email, ChainAddress: persist.NewChainAddress("0x8914496dc01efcc49a2fa340331fb90969b6f1d2", persist.ChainETH)})
 	if err != nil {
 		t.Fatalf("failed to create user: %s", err)
 	}
 
 	email2 := testUser2.Email.String()
-	userID2, err := repos.UserRepository.Create(ctx, persist.CreateUserInput{Username: testUser2.Username.String, Email: &email2})
+	userID2, err := repos.UserRepository.Create(ctx, persist.CreateUserInput{Username: testUser2.Username.String, Email: &email2, ChainAddress: persist.NewChainAddress("0x9a3f9764b21adaf3c6fdf6f947e6d3340a3f8ac5", persist.ChainETH)})
 	if err != nil {
 		t.Fatalf("failed to create user: %s", err)
 	}
