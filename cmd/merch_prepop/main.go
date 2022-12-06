55dc91141b47ad2cdd283e7cd9bd3707c6ec08d8
bennycio <benny@bennyc.io>
2022-12-06 12:39:35 -0800

    prepop script

---
+++ cmd/merch_prepop/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/mikeydub/go-gallery/service/persist"
+	"github.com/mikeydub/go-gallery/service/persist/postgres"
+	"github.com/spf13/viper"
+)
+
+type discountCodes struct {
+	TShirts []string `json:"t_shirts"`
+	Hats    []string `json:"hats"`
+	Cards   []string `json:"cards"`
+}
+
+func main() {
+
+	setDefaults()
+
+	start := time.Now()
+	defer func() {
+		elapsed := time.Since(start)
+		fmt.Printf("Took %s", elapsed)
+	}()
+
+	pg := postgres.NewPgxClient()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Minute)
+	defer cancel()
+
+	discountCodeFile, err := os.Open("discount_codes.json")
+	if err != nil {
+		panic(err)
+	}
+
+	var discountCodes discountCodes
+	err = json.NewDecoder(discountCodeFile).Decode(&discountCodes)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, code := range discountCodes.TShirts {
+		_, err = pg.Exec(ctx, "INSERT INTO discount_codes (id, discount_code, object_type) VALUES ($1, $2, $3)", persist.GenerateID(), code, 0)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	for _, code := range discountCodes.Hats {
+		_, err = pg.Exec(ctx, "INSERT INTO discount_codes (id, discount_code, object_type) VALUES ($1, $2, $3)", persist.GenerateID(), code, 1)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	for _, code := range discountCodes.Cards {
+		_, err = pg.Exec(ctx, "INSERT INTO discount_codes (id, discount_code, object_type) VALUES ($1, $2, $3)", persist.GenerateID(), code, 2)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+}
+
+func setDefaults() {
+	viper.SetDefault("POSTGRES_HOST", "0.0.0.0")
+	viper.SetDefault("POSTGRES_PORT", 5432)
+	viper.SetDefault("POSTGRES_USER", "postgres")
+	viper.SetDefault("POSTGRES_PASSWORD", "")
+	viper.SetDefault("POSTGRES_DB", "postgres")
+
+	viper.AutomaticEnv()
+}
