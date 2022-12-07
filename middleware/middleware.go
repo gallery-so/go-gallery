f2cf72c2f5e27466ae3aa78226baddc3e2957b11
Ezra Smith <ezrasmith@gmail.com>
2022-12-07 10:16:04 -0500

    Don't send traces for OPTIONS requests

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -245,6 +245,11 @@ func Tracing() gin.HandlerFunc {
 			sentry.ContinueFromRequest(c.Request),
 		)
 
+		if c.Request.Method == "OPTIONS" {
+			// Don't sample OPTIONS requests; there's nothing to trace and they eat up our Sentry quota
+			span.Sampled = sentry.SampledFalse
+		}
+
 		defer tracing.FinishSpan(span)
 
 		c.Request = c.Request.WithContext(ctx)
