a5e3f6f9d414b1de06c9a3d86d5f2d788af839cb
jarrel <jarrel.biscocho@gmail.com>
2022-12-08 16:13:24 -0800

    Refactor indexer env setup

--- indexer/cmd/root.go
+++ indexer/cmd/root.go
@@ -10,6 +10,7 @@ import (
 	"google.golang.org/appengine"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
 var (
@@ -21,17 +22,35 @@ var (
 	serviceKeyFile string
 )
 
+func init() {
+	cobra.OnInitialize(indexer.SetDefaults)
+
+	rootCmd.PersistentFlags().BoolVarP(&enableRPC, "enable-rpc", "r", false, "always enable RPC calls")
+	rootCmd.PersistentFlags().BoolVarP(&quietLogs, "quiet", "q", false, "hide debug logs")
+	rootCmd.Flags().Uint64VarP(&port, "port", "p", 4000, "port to serve on")
+	rootCmd.Flags().Uint64VarP(&fromBlock, "from-block", "f", 0, "first block to process")
+	rootCmd.Flags().Uint64VarP(&toBlock, "to-block", "t", 0, "last block to process")
+	rootCmd.MarkFlagsRequiredTogether("from-block", "to-block")
+
+	rootCmd.AddCommand(serverCmd)
+	serverCmd.Flags().Uint64VarP(&port, "port", "p", 6000, "port to serve on")
+	serverCmd.Flags().StringVarP(&serviceKeyFile, "key-file", "f", "./_deploy/service-key-dev.json", "local service key file to use")
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "indexer",
 	Short: "Retrieve all NFTs that have ever been minted",
 	Long: `An NFT indexer lovingly built by your friends at Gallery.
                 Source code is available at https://github.com/gallery-so/go-gallery.`,
 	Args: func(cmd *cobra.Command, args []string) error {
+		indexer.LoadConfigFile("indexer")
+		indexer.ValidateEnv()
+
 		if toBlock < fromBlock {
 			return fmt.Errorf("[from-block] must be less than [to-block]")
 		}
 
-		if !cmd.Flags().Lookup("to-block").Changed && !enableRPC {
+		if !cmd.Flags().Lookup("to-block").Changed && (!enableRPC && viper.GetString("ENV") != "production") {
 			return fmt.Errorf("`flags in group [from-block, to-block] must all be set when [enable-rpc] is not set")
 		}
 
@@ -74,19 +93,6 @@ var serverCmd = &cobra.Command{
 			http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 		}
 	},
-}
-
-func init() {
-	rootCmd.PersistentFlags().BoolVarP(&enableRPC, "enable-rpc", "r", false, "always enable RPC calls")
-	rootCmd.PersistentFlags().BoolVarP(&quietLogs, "quiet", "q", false, "hide debug logs")
-	rootCmd.Flags().Uint64VarP(&port, "port", "p", 4000, "port to serve on")
-	rootCmd.Flags().Uint64VarP(&fromBlock, "from-block", "f", 0, "first block to process")
-	rootCmd.Flags().Uint64VarP(&toBlock, "to-block", "t", 0, "last block to process")
-	rootCmd.MarkFlagsRequiredTogether("from-block", "to-block")
-
-	rootCmd.AddCommand(serverCmd)
-	serverCmd.Flags().Uint64VarP(&port, "port", "p", 6000, "port to serve on")
-	serverCmd.Flags().StringVarP(&serviceKeyFile, "key-file", "f", "./_deploy/service-key-dev.json", "local service key file to use")
 }
 
 func Execute() {
