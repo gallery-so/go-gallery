3852b09a80045af69d74d39a576c1dc5f36777be
bennycio <benny@bennyc.io>
2022-12-13 12:46:32 -0800

    graphQL ID

--- graphql/generated/generated.go
+++ graphql/generated/generated.go
@@ -533,6 +533,7 @@ type ComplexityRoot struct {
 
 	MerchToken struct {
 		DiscountCode func(childComplexity int) int
+		ID           func(childComplexity int) int
 		ObjectType   func(childComplexity int) int
 		Redeemed     func(childComplexity int) int
 		TokenID      func(childComplexity int) int
@@ -2823,6 +2824,13 @@ func (e *executableSchema) Complexity(typeName, field string, childComplexity in
 		}
 
 		return e.complexity.MerchToken.DiscountCode(childComplexity), true
+
+	case "MerchToken.id":
+		if e.complexity.MerchToken.ID == nil {
+			break
+		}
+
+		return e.complexity.MerchToken.ID(childComplexity), true
 
 	case "MerchToken.objectType":
 		if e.complexity.MerchToken.ObjectType == nil {
@@ -5717,7 +5725,8 @@ enum MerchType {
   Card
 }
 
-type MerchToken {
+type MerchToken @goGqlId(fields: ["tokenId"]) {
+  id: ID! 
   tokenId: String!
   objectType: MerchType!
   discountCode: String
@@ -15428,6 +15437,41 @@ func (ec *executionContext) _MerchDiscountCode_tokenId(ctx context.Context, fiel
 	res := resTmp.(*string)
 	fc.Result = res
 	return ec.marshalOString2ᚖstring(ctx, field.Selections, res)
+}
+
+func (ec *executionContext) _MerchToken_id(ctx context.Context, field graphql.CollectedField, obj *model.MerchToken) (ret graphql.Marshaler) {
+	defer func() {
+		if r := recover(); r != nil {
+			ec.Error(ctx, ec.Recover(ctx, r))
+			ret = graphql.Null
+		}
+	}()
+	fc := &graphql.FieldContext{
+		Object:     "MerchToken",
+		Field:      field,
+		Args:       nil,
+		IsMethod:   false,
+		IsResolver: false,
+	}
+
+	ctx = graphql.WithFieldContext(ctx, fc)
+	resTmp, err := ec.ResolverMiddleware(ctx, func(rctx context.Context) (interface{}, error) {
+		ctx = rctx // use context from middleware stack in children
+		return obj.ID, nil
+	})
+	if err != nil {
+		ec.Error(ctx, err)
+		return graphql.Null
+	}
+	if resTmp == nil {
+		if !graphql.HasFieldError(ctx, fc) {
+			ec.Errorf(ctx, "must not be null")
+		}
+		return graphql.Null
+	}
+	res := resTmp.(model.GqlID)
+	fc.Result = res
+	return ec.marshalNID2githubᚗcomᚋmikeydubᚋgoᚑgalleryᚋgraphqlᚋmodelᚐGqlID(ctx, field.Selections, res)
 }
 
 func (ec *executionContext) _MerchToken_tokenId(ctx context.Context, field graphql.CollectedField, obj *model.MerchToken) (ret graphql.Marshaler) {
@@ -32718,6 +32762,16 @@ func (ec *executionContext) _MerchToken(ctx context.Context, sel ast.SelectionSe
 		switch field.Name {
 		case "__typename":
 			out.Values[i] = graphql.MarshalString("MerchToken")
+		case "id":
+			innerFunc := func(ctx context.Context) (res graphql.Marshaler) {
+				return ec._MerchToken_id(ctx, field, obj)
+			}
+
+			out.Values[i] = innerFunc(ctx)
+
+			if out.Values[i] == graphql.Null {
+				invalids++
+			}
 		case "tokenId":
 			innerFunc := func(ctx context.Context) (res graphql.Marshaler) {
 				return ec._MerchToken_tokenId(ctx, field, obj)
