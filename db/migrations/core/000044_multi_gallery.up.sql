af13697d61b0d5bc8c048dba1e960106d3e2842a
bennycio <benny@bennyc.io>
2022-12-22 13:57:43 -0800

    migration works

--- db/migrations/core/000044_multi_gallery.up.sql
+++ db/migrations/core/000044_multi_gallery.up.sql
@@ -1,8 +1,15 @@
 alter table galleries add column if not exists name varchar not null default '';
 alter table galleries add column if not exists description varchar not null default '';
 alter table galleries add column if not exists hidden boolean not null default false;
-alter table galleries add column if not exists position varchar not null;
+
+alter table galleries add column if not exists position varchar;
 alter table users add column if not exists featured_gallery varchar;
-alter table collections add column if not exists gallery_id varchar not null references galleries(id);
+alter table collections add column if not exists gallery_id varchar references galleries(id);
+
+update galleries set position = 'a0' where position is null;
+alter table galleries alter column position set not null;
+
+update collections set gallery_id = (select id from galleries where collections.id = any(galleries.collections) and galleries.deleted = false) where gallery_id is null;
+alter table collections alter column gallery_id set not null;
 
 create unique index if not exists position_idx on galleries (owner_user_id, position) where deleted = false;
