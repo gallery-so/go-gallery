e0355cde1afbc8e4d2ebb78ec921c2b86ee70fee
bennycio <benny@bennyc.io>
2023-01-17 11:23:52 -0800

    only not deleted users

--- db/migrations/core/000046_multi_gallery_adjustments.up.sql
+++ db/migrations/core/000046_multi_gallery_adjustments.up.sql
@@ -1,4 +1,4 @@
 drop index if exists position_idx;
 alter table galleries add constraint position_cst unique (owner_user_id, position) deferrable;
 
-update users set featured_gallery = (select id from galleries where galleries.owner_user_id = users.id and galleries.deleted = false limit 1);
+update users set featured_gallery = (select id from galleries where galleries.owner_user_id = users.id and galleries.deleted = false limit 1) where deleted = false;
