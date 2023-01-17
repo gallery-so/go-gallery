3ab9b28b8eaa0feff01a8a452ce85fbcf6daf32d
bennycio <benny@bennyc.io>
2023-01-17 11:22:40 -0800

    adjust schema for updates and featured gallery

---
+++ db/migrations/core/000046_multi_gallery_adjustments.up.sql
@@ -0,0 +1,4 @@
+drop index if exists position_idx;
+alter table galleries add constraint position_cst unique (owner_user_id, position) deferrable;
+
+update users set featured_gallery = (select id from galleries where galleries.owner_user_id = users.id and galleries.deleted = false limit 1);
