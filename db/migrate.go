b7da5d43a8830873ac68352485ab1275b2cef436
Ezra Smith <ezrasmith@gmail.com>
2023-01-31 15:46:46 -0500

    Allow migrations up to a specific version number

--- db/migrate.go
+++ db/migrate.go
@@ -9,18 +9,9 @@ import (
 	"github.com/mikeydub/go-gallery/util"
 )
 
+// RunMigration runs all migrations in the specified directory
 func RunMigration(client *sql.DB, file string) error {
-	dir, err := util.FindFile(file, 3)
-	if err != nil {
-		return err
-	}
-
-	d, err := pgdriver.WithInstance(client, &pgdriver.Config{})
-	if err != nil {
-		return err
-	}
-
-	m, err := migrate.NewWithDatabaseInstance("file://"+dir, "postgres", d)
+	m, err := newMigrateInstance(client, file)
 	if err != nil {
 		return err
 	}
@@ -31,4 +22,39 @@ func RunMigration(client *sql.DB, file string) error {
 	}
 
 	return nil
+}
+
+// RunMigrationToVersion runs migrations in the specified directory, up to (and including) the
+// specified migration version number
+func RunMigrationToVersion(client *sql.DB, file string, toVersion uint) error {
+	m, err := newMigrateInstance(client, file)
+	if err != nil {
+		return err
+	}
+
+	err = m.Migrate(toVersion)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func newMigrateInstance(client *sql.DB, file string) (*migrate.Migrate, error) {
+	dir, err := util.FindFile(file, 3)
+	if err != nil {
+		return nil, err
+	}
+
+	d, err := pgdriver.WithInstance(client, &pgdriver.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	m, err := migrate.NewWithDatabaseInstance("file://"+dir, "postgres", d)
+	if err != nil {
+		return nil, err
+	}
+
+	return m, nil
 }
