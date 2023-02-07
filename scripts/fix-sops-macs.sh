f629966103f01f925db57adb1df1cc2e3ec0fac3
Ezra Smith <ezrasmith@gmail.com>
2023-02-07 16:33:23 -0500

    Add script to fix sops MACs

---
+++ scripts/fix-sops-macs.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Very helpful script taken from: https://github.com/mozilla/sops/issues/52#issuecomment-726807596
+
+set -o errexit
+set -o pipefail
+set -o nounset
+
+find . -name '*.yaml' | while read -r file;
+do
+    sops -d "$file" >/dev/null 2>&1 && rc=$? || rc=$?
+    # In case of MAC mismatch, then MAC is regenerated
+    # See https://github.com/mozilla/sops/blob/v3.6.1/cmd/sops/codes/codes.go#L19
+    if [ $rc -eq 51  ] ; then
+      echo "Regenerating sops MAC for: $file"
+      EDITOR="vim -es +'norm Go' +':wq'"  sops --ignore-mac "$file"
+    fi
+done
