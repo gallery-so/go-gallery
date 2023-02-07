df412c29fa8f6af5e9ab15417263da15e5226007
Benjamin Conn <benny@bennyc.io>
2023-02-07 12:42:38 -0800

    set permissions

--- db/migrations/core/000058_social_accounts.up.sql
+++ db/migrations/core/000058_social_accounts.up.sql
@@ -1,10 +1,10 @@
 alter table pii.for_users add column if not exists pii_external_socials jsonb not null default '{}';
+
 drop view if exists pii.user_view;
 create or replace view pii.user_view as
     select users.*, for_users.pii_email_address, for_users.pii_external_socials from users left join pii.for_users on users.id = for_users.user_id and for_users.deleted = false;
 
 
--- will later be in the pii schema
 create table if not exists pii.social_account_auth (
     id varchar(255) not null primary key,
     deleted boolean default false not null,
@@ -16,5 +16,9 @@ create table if not exists pii.social_account_auth (
     access_token varchar,
     refresh_token varchar
 );
+
+grant select on pii.social_account_auth to access_ro_pii;
+grant all on pii.social_account_auth to access_rw_pii;
+alter table pii.social_account_auth owner to access_rw_pii;
 
 create unique index if not exists social_account_auth_user_id_provider_idx on pii.social_account_auth(user_id, provider);
