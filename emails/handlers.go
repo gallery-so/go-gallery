31908a62d34dac2c2a70e2279f5ea3abe17534b3
robin <rokim8@gmail.com>
2023-02-13 16:36:40 -0500

    Reduce rate limit for email validation endpoint (#746)
    * reduce
    
    * ezra updates

--- emails/handlers.go
+++ emails/handlers.go
@@ -1,26 +1,30 @@
 package emails
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
+	"github.com/go-redis/redis/v8"
 	"github.com/mikeydub/go-gallery/db/gen/coredb"
 	"github.com/mikeydub/go-gallery/graphql/dataloader"
 	"github.com/mikeydub/go-gallery/middleware"
 	"github.com/sendgrid/sendgrid-go"
 )
 
-func handlersInitServer(router *gin.Engine, loaders *dataloader.Loaders, queries *coredb.Queries, s *sendgrid.Client, lim *middleware.KeyRateLimiter) *gin.Engine {
-
+func handlersInitServer(router *gin.Engine, loaders *dataloader.Loaders, queries *coredb.Queries, s *sendgrid.Client, r *redis.Client) *gin.Engine {
 	sendGroup := router.Group("/send")
 
 	sendGroup.POST("/notifications", middleware.AdminRequired(), adminSendNotificationEmail(queries, s))
 
-	sendGroup.POST("/verification", middleware.RateLimited(lim), sendVerificationEmail(loaders, queries, s))
+	verificationLimiter := middleware.RateLimited(middleware.NewKeyRateLimiter(1, time.Second*5, r))
+	sendGroup.POST("/verification", verificationLimiter, sendVerificationEmail(loaders, queries, s))
 
 	router.POST("/subscriptions", updateSubscriptions(queries))
 	router.POST("/unsubscribe", unsubscribe(queries))
 	router.POST("/resubscribe", resubscribe(queries))
 
 	router.POST("/verify", verifyEmail(queries))
-	router.GET("/preverify", middleware.RateLimited(lim), preverifyEmail())
+	preVerifyLimiter := middleware.RateLimited(middleware.NewKeyRateLimiter(1, time.Millisecond*500, r))
+	router.GET("/preverify", preVerifyLimiter, preverifyEmail())
 	return router
 }
