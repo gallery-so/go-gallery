e5c0b5adf8a7c3cacbefc26b56330680c65d8af0
jarrel <jarrel.biscocho@gmail.com>
2023-02-15 16:24:36 -0800

    Add role to migration

--- db/migrations/core/000060_add_gallery_update_to_gallery_edit_idx.up.sql
+++ db/migrations/core/000060_add_gallery_update_to_gallery_edit_idx.up.sql
@@ -1,2 +1,3 @@
+set role to access_rw;
 drop index if exists events_gallery_edit_idx;
 create index if not exists events_gallery_edit_idx on events (created_at, actor_id) where action in ('CollectionCreated', 'CollectorsNoteAddedToCollection', 'CollectorsNoteAddedToToken', 'TokensAddedToCollection', 'GalleryInfoUpdated');
