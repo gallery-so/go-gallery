921ccd4e628c54dcc56673905725acdfe03236f4
jarrel <jarrel.biscocho@gmail.com>
2023-02-27 18:14:55 -0800

    Add login

--- db/migrations/core/000062_add_refresh_recommended_users_view.up.sql
+++ db/migrations/core/000062_add_refresh_recommended_users_view.up.sql
@@ -1,8 +1,8 @@
 ------------------------------------------------------------------------------------
--- NOTE: Migrations that schedule jobs should be applied after the pg_cron
---       extension is created. If the extension hasn't beeen made, run the below
---       statement with the superuser role:
+-- NOTE: Should be applied after the pg_cron extension is created. If the extension
+--       hasn't been made, run the below statement with the superuser role:
 --           `create extension if not exists pg_cron;
---            alter schema cron owner to access_rw;`
+--            alter schema cron owner to access_rw;
+--            alter role access_rw with login;`
 ------------------------------------------------------------------------------------
 select cron.schedule('daily', 'refresh materialized view concurrently top_recommended_users with data');
