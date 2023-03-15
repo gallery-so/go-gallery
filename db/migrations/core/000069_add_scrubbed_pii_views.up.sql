ec77b11e92162b30b4fcb23301adce90ca386705
Ezra Smith <ezrasmith@gmail.com>
2023-03-15 17:25:00 -0400

    Rework scrubbed_pii views migration to make sqlc happy

--- db/migrations/core/000069_add_scrubbed_pii_views.up.sql
+++ db/migrations/core/000069_add_scrubbed_pii_views.up.sql
@@ -1,31 +1,33 @@
 set role to access_rw_pii;
 
+drop view if exists scrubbed_pii.for_users;
 create view scrubbed_pii.for_users as (
+    with socials_kvp as (
+        -- Redundant jsonb_each because sqlc throws an error if we select "(jsonb_each(pii_socials)).*"
+        select user_id, (jsonb_each(pii_socials)).key as key, (jsonb_each(pii_socials)).value as value from pii.for_users
+    ),
+
+    socials_scrubbed as (
+        select user_id, socials_kvp.key as k,
+        case
+            when (socials_kvp.value -> 'display')::bool then socials_kvp.value
+            else '{"display":false, "metadata":{}}'::jsonb ||
+                jsonb_build_object('provider', socials_kvp.value -> 'provider') ||
+                jsonb_build_object('id', users.username_idempotent || '-dummy-id')
+        end as v
+        from socials_kvp join users on socials_kvp.user_id = users.id
+    ),
+
+    socials_aggregated as (
+        select user_id, jsonb_object_agg(k, v) as socials
+            from socials_scrubbed
+            group by user_id
+    ),
+
     -- includes social data when display = true, otherwise makes a dummy id and omits metadata
-    with scrubbed_socials as (
-        with socials as (
-            select user_id, (jsonb_each(pii_socials)).* from pii.for_users
-        ),
-
-        scrubbed as (
-            select user_id, key as k,
-            case
-                when (socials.value -> 'display')::bool then socials.value
-                else '{"display":false, "metadata":{}}'::jsonb ||
-                    jsonb_build_object('provider', socials.value -> 'provider') ||
-                    jsonb_build_object('id', users.username_idempotent || '-dummy-id')
-            end as v
-            from socials join users on socials.user_id = users.id
-        ),
-
-        aggregated as (
-            select user_id, jsonb_object_agg(k, v) as socials
-                from scrubbed
-                group by user_id
-        )
-
-        select for_users.user_id, coalesce(aggregated.socials, '{}'::jsonb) as scrubbed_socials from pii.for_users
-            left join aggregated on aggregated.user_id = for_users.user_id
+    scrubbed_socials as (
+        select for_users.user_id, coalesce(socials_aggregated.socials, '{}'::jsonb) as scrubbed_socials from pii.for_users
+            left join socials_aggregated on socials_aggregated.user_id = for_users.user_id
     ),
 
     -- <username>@dummy-email.gallery.so for users who have email addresses, null otherwise
