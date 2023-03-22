25e1693fd095d47c408976340cc0480c49e4540b
Ezra Smith <ezrasmith@gmail.com>
2023-03-22 12:46:59 -0400

    Add scrubbed_pii view

--- db/migrations/core/000072_add_pii_account_creation_info.up.sql
+++ db/migrations/core/000072_add_pii_account_creation_info.up.sql
@@ -1,3 +1,5 @@
+set role to access_rw_pii;
+
 create table if not exists pii.account_creation_info (
     user_id varchar(255) primary key references users(id),
     ip_address text not null,
@@ -5,3 +7,14 @@ create table if not exists pii.account_creation_info (
 );
 
 select cron.schedule('purge-account-creation-info', '@weekly', 'delete from pii.account_creation_info where created_at < now() - interval ''180 days''');
+
+-- Create a scrubbed_pii view of the table
+drop view if exists scrubbed_pii.account_creation_info;
+create view scrubbed_pii.account_creation_info as (
+    -- Doing this limit 0 union ensures we have appropriate column types for our view
+    (select * from pii.account_creation_info limit 0)
+    union
+    select user_id, 'scrubbed', created_at from pii.account_creation_info
+);
+
+set role to access_rw;
