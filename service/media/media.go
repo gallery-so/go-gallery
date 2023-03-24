f8b6dd6957f083ee9a4a059ff84872f122f7b430
Benny <benny@bennyc.io>
2023-03-24 16:01:02 -0700

    start with ipfs if gateway (#877)
    * start with ipfs if gateway
    
    * separate out cases
    
    * comma

--- service/media/media.go
+++ service/media/media.go
@@ -929,14 +929,22 @@ func PredictMediaType(pCtx context.Context, url string) (persist.MediaType, stri
 		return persist.MediaTypeSVG, "image/svg", int64(len(asURI.String())), nil
 	case persist.URITypeBase64BMP:
 		return persist.MediaTypeBase64BMP, "image/bmp", int64(len(asURI.String())), nil
-	case persist.URITypeHTTP, persist.URITypeIPFSAPI, persist.URITypeIPFSGateway:
-		contentType, contentLength, err := rpc.GetHTTPHeaders(pCtx, url)
+	case persist.URITypeIPFS:
+		contentType, contentLength, err := rpc.GetIPFSHeaders(pCtx, strings.TrimPrefix(asURI.String(), "ipfs://"))
 		if err != nil {
 			return persist.MediaTypeUnknown, "", 0, err
 		}
 		return persist.MediaFromContentType(contentType), contentType, contentLength, nil
-	case persist.URITypeIPFS:
-		contentType, contentLength, err := rpc.GetIPFSHeaders(pCtx, strings.TrimPrefix(asURI.String(), "ipfs://"))
+	case persist.URITypeIPFSGateway:
+		contentType, contentLength, err := rpc.GetIPFSHeaders(pCtx, util.GetURIPath(asURI.String(), false))
+		if err == nil {
+			return persist.MediaFromContentType(contentType), contentType, contentLength, nil
+		} else if err != nil {
+			logger.For(pCtx).Errorf("could not get IPFS headers for %s: %s", url, err)
+		}
+		fallthrough
+	case persist.URITypeHTTP, persist.URITypeIPFSAPI:
+		contentType, contentLength, err := rpc.GetHTTPHeaders(pCtx, url)
 		if err != nil {
 			return persist.MediaTypeUnknown, "", 0, err
 		}
