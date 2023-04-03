35aa91ab973df5da6dae2adac764f65eb1cf2818
jacohend <jacob@tierion.com>
2023-04-03 15:35:26 -0400

    I swar

--- service/persist/postgres/token.go
+++ service/persist/postgres/token.go
@@ -371,7 +371,7 @@ func (t *TokenRepository) upsertERC721Tokens(pCtx context.Context, pTokens []per
 	// Postgres only allows 65535 parameters at a time.
 	// TODO: Consider trying this implementation at some point instead of chunking:
 	//       https://klotzandrew.com/blog/postgres-passing-65535-parameter-limit
-	paramsPerRow := 20
+	paramsPerRow := 19
 	rowsPerQuery := 65535 / paramsPerRow
 
 	if len(pTokens) > rowsPerQuery {
