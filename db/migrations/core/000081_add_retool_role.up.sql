9bca4dacead2a62fce098755bdb5a0459c9e7ef7
jarrel <jarrel.biscocho@gmail.com>
2023-05-03 11:24:58 -0700

    Change to rw

--- db/migrations/core/000081_add_retool_role.up.sql
+++ db/migrations/core/000081_add_retool_role.up.sql
@@ -1,5 +1,5 @@
 /* {% require_sudo %} */
 drop role if exists gallery_retool;
 create role gallery_retool noinherit login;
-grant access_rw to gallery_retool;
-alter role gallery_retool set role to access_rw;
+grant access_ro to gallery_retool;
+alter role gallery_retool set role to access_ro;
