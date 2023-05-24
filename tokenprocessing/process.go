35aa4241eb9b5ab6dfb481962a58d739f8b349db
Benny <benny@bennyc.io>
2023-05-23 19:16:02 -0700

    Benny/token lock (#981)
    * throttle tokens
    
    * no prefix
    
    * lock just inside goroutine

--- tokenprocessing/process.go
+++ tokenprocessing/process.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
-	"sort"
 	"time"
 
 	"github.com/mikeydub/go-gallery/db/gen/coredb"
@@ -43,31 +42,12 @@ func processMediaForUsersTokens(tp *tokenProcessor, tokenRepo *postgres.TokenGal
 
 		reqCtx := logger.NewContextWithFields(c.Request.Context(), logrus.Fields{"userID": input.UserID})
 
-		sort.Slice(input.Chains, func(i, j int) bool {
-			return input.Chains[i] < input.Chains[j]
-		})
-
-		lockID := input.UserID.String()
-		for _, chain := range input.Chains {
-			lockID += fmt.Sprintf(":%d", chain)
-		}
-		// V3Migration: Remove when migration is complete
-		if input.IsV3 {
-			lockID += ":v3"
-		}
-
-		if err := throttler.Lock(reqCtx, lockID); err != nil {
-			// Reply with a non-200 status so that the message is tried again later on
-			util.ErrResponse(c, http.StatusTooManyRequests, err)
-			return
-		}
-		defer throttler.Unlock(reqCtx, input.UserID.String())
-
 		wp := pool.New().WithMaxGoroutines(50).WithErrors()
 
 		logger.For(reqCtx).Infof("Processing Media: %s - Started (%d tokens)", input.UserID, len(input.TokenIDs))
 
 		for _, tokenID := range input.TokenIDs {
+
 			t, err := tokenRepo.GetByID(reqCtx, tokenID)
 			if err != nil {
 				logger.For(reqCtx).Errorf("failed to fetch tokenID=%s: %s", tokenID, err)
@@ -79,7 +59,18 @@ func processMediaForUsersTokens(tp *tokenProcessor, tokenRepo *postgres.TokenGal
 				logger.For(reqCtx).Errorf("Error getting contract: %s", err)
 			}
 
+			lockID := tokenID.String()
+			// V3Migration: Remove when migration is complete
+			if input.IsV3 {
+				lockID += ":v3"
+			}
 			wp.Go(func() error {
+				if err := throttler.Lock(reqCtx, lockID); err != nil {
+					logger.For(reqCtx).Errorf("failed to lock tokenID=%s: %s", tokenID, err)
+					return err
+				}
+				defer throttler.Unlock(reqCtx, lockID)
+
 				ctx := sentryutil.NewSentryHubContext(reqCtx)
 				err := tp.ProcessTokenPipeline(reqCtx, t, contract, "", persist.ProcessingCauseSync)
 				if err != nil {
