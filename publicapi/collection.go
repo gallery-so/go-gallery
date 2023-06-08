91cbf6d7d15a3ff9bf789268c87aa27fb1148a7c
jarrel <jarrel.biscocho@gmail.com>
2023-06-08 16:44:43 -0700

    Lower TTL

--- publicapi/collection.go
+++ publicapi/collection.go
@@ -131,7 +131,7 @@ func (api CollectionAPI) GetTopCollectionsForCommunity(ctx context.Context, chai
 		r := ranker{
 			Cache: redis.NewCache(redis.SearchCache),
 			Key:   fmt.Sprintf("top_collections_by_address:%d:%s", chainAddress.Chain(), chainAddress.Address()),
-			TTL:   time.Hour * 6,
+			TTL:   time.Hour * 2,
 			CalcFunc: func(ctx context.Context) ([]persist.DBID, error) {
 				return api.queries.GetTopCollectionsForCommunity(ctx, db.GetTopCollectionsForCommunityParams{
 					Chain:   chainAddress.Chain(),
