7a1e89e5fad9fa226499579601d4fa7e2e2ae35f
Rohan-cp <rohancp9@icloud.com>
2023-07-19 11:49:20 -0400

    revert unintended change

--- publicapi/gallery.go
+++ publicapi/gallery.go
@@ -660,7 +660,6 @@ func (api GalleryAPI) ViewGallery(ctx context.Context, galleryID persist.DBID) (
 	return gallery, nil
 }
 
-
 func getExternalID(ctx context.Context) *string {
 	gc := util.MustGetGinContext(ctx)
 	if ip := net.ParseIP(gc.ClientIP()); ip != nil && !ip.IsPrivate() {
