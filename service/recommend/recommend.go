8d19f9d5609e27ba68976604da369bb85417a92d
jarrel <jarrel.biscocho@gmail.com>
2023-08-02 18:47:25 -0700

    Update recommend.go (#1117)

--- service/recommend/recommend.go
+++ service/recommend/recommend.go
@@ -9,7 +9,6 @@ import (
 	"github.com/gin-gonic/gin"
 
 	db "github.com/mikeydub/go-gallery/db/gen/coredb"
-	"github.com/mikeydub/go-gallery/service/logger"
 	"github.com/mikeydub/go-gallery/service/persist"
 	"github.com/mikeydub/go-gallery/util"
 )
