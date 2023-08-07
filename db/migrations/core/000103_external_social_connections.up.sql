2424a89ac9df1e66255121fbe8a4873b3dd106ab
Benjamin Conn <benny@bennyc.io>
2023-08-07 14:58:39 -0700

    backfill connections

---
+++ db/migrations/core/000103_external_social_connections.up.sql
@@ -0,0 +1,12 @@
+create table if not exists external_social_connections (
+    id varchar(255) primary key,
+    version int not null default 0,
+    social_account_type varchar(255) not null,
+    follower_id varchar(255) references users(id) not null,
+    followee_id varchar(255) references users(id) not null,
+    created_at timestamptz not null default current_timestamp,
+    last_updated timestamptz not null default current_timestamp,
+    deleted boolean not null default false
+);
+
+create unique index if not exists external_social_connections_social_account_type_follower_social_id_idx on external_social_connections (social_account_type, follower_social_id) where (deleted = false);
