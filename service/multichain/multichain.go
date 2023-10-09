f69d3d6721cb783b0d7dea95a44c810f507adb2f
Benjamin Conn <benny@bennyc.io>
2023-10-09 12:06:42 -0700

    no unowning for now

--- service/multichain/multichain.go
+++ service/multichain/multichain.go
@@ -320,7 +320,7 @@ func (p *Provider) SyncTokensByUserID(ctx context.Context, userID persist.DBID, 
 		wg.Wait()
 	}()
 
-	_, err = p.receiveSyncedTokensForUser(ctx, user, chains, incomingTokens, incomingContracts, errChan, true)
+	_, err = p.receiveSyncedTokensForUser(ctx, user, chains, incomingTokens, incomingContracts, errChan, false)
 	return err
 }
 
