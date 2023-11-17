306826477242ff31062b171911127718930e5aa7
jarrel <jarrel.biscocho@gmail.com>
2023-11-17 10:16:17 -0800

    Add first time post indicator (#1288)

--- feedbot/server.go
+++ feedbot/server.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -81,7 +82,18 @@ func postToSlack(gql *graphql.Client) gin.HandlerFunc {
 		contextBlock := map[string]any{}
 		contextBlock["type"] = "context"
 		contextBlock["elements"] = make([]any, 0)
-		contextBlock["elements"] = append(contextBlock["elements"].([]any), textObject(fmt.Sprintf("New Post - *%s*", template.PostOrError.Post.Tokens[0].Community.Name)))
+
+		var firstPostID string
+		if len(template.PostOrError.Post.Author.Feed.Edges) == 1 {
+			firstPostID = template.PostOrError.Post.Author.Feed.Edges[0].Node.Post.DBID
+		}
+
+		if message.PostID.String() == firstPostID {
+			emoji := randomEmoji()
+			contextBlock["elements"] = append(contextBlock["elements"].([]any), textObject(fmt.Sprintf("%s *We got a first time poster!!!* %s - *%s*", emoji, emoji, template.PostOrError.Post.Tokens[0].Community.Name)))
+		} else {
+			contextBlock["elements"] = append(contextBlock["elements"].([]any), textObject(fmt.Sprintf("New Post - *%s*", template.PostOrError.Post.Tokens[0].Community.Name)))
+		}
 
 		tokenPFP := template.PostOrError.Post.Author.ProfileImage.TokenProfileImage.Token.Media.Media.PreviewURLs.Thumbnail
 		fallbackPFP := template.PostOrError.Post.Author.ProfileImage.TokenProfileImage.Token.Media.Media.FallbackMedia.MediaURL
@@ -190,6 +202,15 @@ type slackTemplateInfo struct {
 						}
 					} `graphql:"...on EnsProfileImage"`
 				}
+				Feed struct {
+					Edges []struct {
+						Node struct {
+							Post struct {
+								DBID string
+							} `graphql:"...on Post"`
+						}
+					}
+				} `graphql:"feed(first: 1)"`
 			}
 			Caption string
 			Tokens  []struct {
@@ -230,4 +251,18 @@ func linkButtonObject(buttonText, url string) map[string]any {
 		"text": map[string]any{"type": "plain_text", "text": buttonText},
 		"url":  url,
 	}
+}
+
+var hypeEmojis = []string{
+	":admire_hype:",
+	":get_hype:",
+	":celebrate:",
+	":lfg:",
+	":boy-dancing:",
+	":lets_go:",
+	":blob-hearts:",
+}
+
+func randomEmoji() string {
+	return hypeEmojis[rand.Intn(len(hypeEmojis))]
 }
