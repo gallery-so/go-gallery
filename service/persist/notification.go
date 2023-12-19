58cf9981dc7c78b323e67196d67612623c37e9e7
Benjamin Conn <benny@bennyc.io>
2023-12-19 15:50:06 -0800

    event data

--- service/persist/notification.go
+++ service/persist/notification.go
@@ -4,18 +4,30 @@ import (
 	"github.com/mikeydub/go-gallery/util"
 )
 
+type AnnouncementDetails struct {
+	Platform             string `json:"platform"`
+	InternalID           string `json:"internal_id"`
+	ImageURL             string `json:"image_url,omitempty"`
+	Title                string `json:"title,omitempty"`
+	Description          string `json:"description,omitempty"`
+	CTAText              string `json:"cta_text,omitempty"`
+	CTALink              string `json:"cta_link,omitempty"`
+	PushNotificationText string `json:"push_notification_text,omitempty"`
+}
+
 type NotificationData struct {
-	AuthedViewerIDs        []DBID    `json:"viewer_ids,omitempty"`
-	UnauthedViewerIDs      []string  `json:"unauthed_viewer_ids,omitempty"`
-	FollowerIDs            []DBID    `json:"follower_ids,omitempty"`
-	AdmirerIDs             []DBID    `json:"admirer_ids,omitempty"`
-	FollowedBack           NullBool  `json:"followed_back,omitempty"`
-	Refollowed             NullBool  `json:"refollowed,omitempty"`
-	NewTokenID             DBID      `json:"new_token_id,omitempty"`
-	NewTokenQuantity       HexString `json:"new_token_quantity,omitempty"`
-	OriginalCommentID      DBID      `json:"original_comment_id,omitempty"`
-	ActivityBadgeThreshold int       `json:"activity_badge_threshold,omitempty"`
-	NewTopActiveUser       bool      `json:"new_top_active_user,omitempty"`
+	AuthedViewerIDs        []DBID               `json:"viewer_ids,omitempty"`
+	UnauthedViewerIDs      []string             `json:"unauthed_viewer_ids,omitempty"`
+	FollowerIDs            []DBID               `json:"follower_ids,omitempty"`
+	AdmirerIDs             []DBID               `json:"admirer_ids,omitempty"`
+	FollowedBack           NullBool             `json:"followed_back,omitempty"`
+	Refollowed             NullBool             `json:"refollowed,omitempty"`
+	NewTokenID             DBID                 `json:"new_token_id,omitempty"`
+	NewTokenQuantity       HexString            `json:"new_token_quantity,omitempty"`
+	OriginalCommentID      DBID                 `json:"original_comment_id,omitempty"`
+	ActivityBadgeThreshold int                  `json:"activity_badge_threshold,omitempty"`
+	NewTopActiveUser       bool                 `json:"new_top_active_user,omitempty"`
+	AnnouncementDetails    *AnnouncementDetails `json:"announcement_details,omitempty"`
 }
 
 func (n NotificationData) Validate() NotificationData {
@@ -31,6 +43,7 @@ func (n NotificationData) Validate() NotificationData {
 	result.Refollowed = n.Refollowed
 	result.ActivityBadgeThreshold = n.ActivityBadgeThreshold
 	result.NewTopActiveUser = n.NewTopActiveUser
+	result.AnnouncementDetails = n.AnnouncementDetails
 
 	return result
 }
@@ -50,6 +63,11 @@ func (n NotificationData) Concat(other NotificationData) NotificationData {
 		return i > 0
 	})
 	result.NewTopActiveUser = other.NewTopActiveUser || n.NewTopActiveUser
+	if other.AnnouncementDetails != nil {
+		result.AnnouncementDetails = other.AnnouncementDetails
+	} else {
+		result.AnnouncementDetails = n.AnnouncementDetails
+	}
 
 	return result.Validate()
 }
