a04c8fa18a1bb30af367ba020380ac149250c967
jarrel <jarrel.biscocho@gmail.com>
2024-03-05 16:30:45 -0800

    Update 000152_add_farcaster_fid_idx.up.sql
    Fix casing on farcaster index

--- db/migrations/core/000152_add_farcaster_fid_idx.up.sql
+++ db/migrations/core/000152_add_farcaster_fid_idx.up.sql
@@ -1,2 +1,2 @@
 /* {% require_sudo %} */
-create index for_users_farcaster_fid_idx on pii.for_users(((pii_socials -> 'farcaster'::text) ->> 'id'::text) text_ops);
+create index for_users_farcaster_fid_idx on pii.for_users(((pii_socials -> 'Farcaster'::text) ->> 'id'::text) text_ops);
