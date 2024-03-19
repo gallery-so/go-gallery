aaf8b5c6cc4a5af6e812e60e30589f6ffe42367f
radazen <radazen@users.noreply.github.com>
2024-03-19 13:48:18 -0400

    Add verified email addresses to the SendGrid marketing list (#1456)

--- emails/emails.go
+++ emails/emails.go
@@ -87,6 +87,7 @@ func setDefaults() {
 	viper.SetDefault("SENDGRID_VALIDATION_KEY", "")
 	viper.SetDefault("FROM_EMAIL", "test@gallery.so")
 	viper.SetDefault("SENDGRID_DEFAULT_LIST_ID", "865cea98-bf23-4ca3-a8d7-2dc9ea29951b")
+	viper.SetDefault("SENDGRID_MARKETING_LIST_ID", "")
 	viper.SetDefault("SENDGRID_NOTIFICATIONS_TEMPLATE_ID", "d-6135d8f36e9946979b0dcf1800363ab4")
 	viper.SetDefault("SENDGRID_VERIFICATION_TEMPLATE_ID", "d-b575d54dc86d40fdbf67b3119589475a")
 	viper.SetDefault("SENDGRID_DIGEST_TEMPLATE_ID", "d-0b9b6b0b0b5e4b6e9b0b0b5e4b6e9b0b")
