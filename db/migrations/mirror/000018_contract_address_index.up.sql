fe695c0c11a8fb9c67c80209c102e508f03c906d
Ezra Smith <ezrasmith@gmail.com>
2024-06-26 10:39:48 -0400

    Remove "concurrently" since migrations appear to be applied in a tx

--- db/migrations/mirror/000018_contract_address_index.up.sql
+++ db/migrations/mirror/000018_contract_address_index.up.sql
@@ -1,9 +1,9 @@
-create index concurrently if not exists ethereum_contracts_address on ethereum.contracts (address);
-create index concurrently if not exists base_contracts_address on base.contracts (address);
-create index concurrently if not exists zora_contracts_address on zora.contracts (address);
-create index concurrently if not exists base_sepolia_contracts_address on base_sepolia.contracts (address);
+create index if not exists ethereum_contracts_address on ethereum.contracts (address);
+create index if not exists base_contracts_address on base.contracts (address);
+create index if not exists zora_contracts_address on zora.contracts (address);
+create index if not exists base_sepolia_contracts_address on base_sepolia.contracts (address);
 
-create index concurrently if not exists ethereum_contracts_owned_by on ethereum.contracts (owned_by);
-create index concurrently if not exists base_contracts_owned_by on base.contracts (owned_by);
-create index concurrently if not exists zora_contracts_owned_by on zora.contracts (owned_by);
-create index concurrently if not exists base_sepolia_contracts_owned_by on base_sepolia.contracts (owned_by);
+create index if not exists ethereum_contracts_owned_by on ethereum.contracts (owned_by);
+create index if not exists base_contracts_owned_by on base.contracts (owned_by);
+create index if not exists zora_contracts_owned_by on zora.contracts (owned_by);
+create index if not exists base_sepolia_contracts_owned_by on base_sepolia.contracts (owned_by);
